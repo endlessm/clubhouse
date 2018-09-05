b2ef23f14c71dbc4daa93ca019a531e925b29237
Joaquim Rocha <jrocha@endlessm.com>
2018-09-05 10:27:49 +0200

    Make the FlappyHack quest useful

--- eosclubhouse/quests/flappyhack.py
+++ eosclubhouse/quests/flappyhack.py
@@ -18,28 +18,67 @@
 #       Joaquim Rocha <jrocha@endlessm.com>
 #
 
+import time
+
 from eosclubhouse.quest import Registry, Quest
+from eosclubhouse.desktop import Desktop, App
 
 
 class FlappyHack(Quest):
 
+    TARGET_APP_DBUS_NAME = 'com.endlessm.hackybird'
+
     def __init__(self):
         super().__init__('FlappyBird Hacking', 'aggretsuko',
-                         ('Betcha cannot beat my super high score in the FlappyHack game! '
+                         ('Betcha cannot beat my score in Hackybird! '
                           'Wanna try it?'))
 
-    def _opened_already(self):
-        print('OPENED')
-
-    def _not_opened_already(self):
-        print('NOT OPENED')
+    def _open_app(self):
+        Desktop.launch_app(self.TARGET_APP_DBUS_NAME)
 
     def start(self):
-        self.show_message('Then make sure that you open FlappyHack!')
-        self.show_message('Bring it on!!!!!', mood='mad')
-        self.show_question('Have you opened it yet?',
-                           choices=[('Yup', self._opened_already),
-                                    ('Not yet…', self._not_opened_already)])
+        self.show_message("Then make sure that you open Hackybird! Let's do it!!!!!", mood='happy')
+        time.sleep(2)
+
+        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
+            self.show_question("Hmm… I don't see Hackybird running… Should open it for you?",
+                               choices=[('Please do!', self._open_app)])
+            while not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
+                time.sleep(1)
+
+        app = App(self.TARGET_APP_DBUS_NAME)
+
+        score = 1
+        self.show_message("Okay… Now try to beat my super mega awesome score of… "
+                          "{} POINT!".format(score), mood='in_love')
+        app.set_object_property('label_name', 'label', 'Aggretsuko')
+        app.set_object_property('label_score', 'label', str(score))
+        app.set_object_property('stack_app', 'visible-child-name', 'page0')
+        app.set_object_property('view_hack', 'score', ('i', score))
+        app.highlight_object('button_start')
+
+        attempts = 0
+        while True:
+            current_score = app.get_object_property('view_hack', 'score')
+            print('Attempts:', attempts, current_score)
+            if current_score > score:
+                if score == 100:
+                    self.show_message("Oh no! You beat me…! Good for you…", mood='angry_sad')
+                    break
+
+                self.show_message("Oh WHAT!? … You think you beat me? Check this out!", mood='mad')
+                time.sleep(2)
+
+                score = 100
+                app.set_object_property('label_name', 'label', 'Aggretsuko')
+                app.set_object_property('label_score', 'label', str(score))
+                app.set_object_property('stack_app', 'visible-child-name', 'page0')
+                self.show_message("WAH-HAHAHA! Now try again…", mood='mad')
+                time.sleep(2)
+            elif attempts == 10:
+                self.show_message("You just have to do {} point…".format(score), mood='in_love')
+            attempts += 1
+            time.sleep(1)
 
 
 Registry.register_quest(FlappyHack)
