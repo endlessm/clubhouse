fb21186b4de1af7b511ea7ae848806c448eb9098
Joaquim Rocha <jrocha@endlessm.com>
2018-09-14 15:22:22 +0200

    fixup! Initial commit
    This commit shall be squashed with the initial one, since it
    fixes a lot of things that were ery experimental (and wrong) in the
    old initial commit.

--- eosclubhouse/clubhouse.py
+++ eosclubhouse/clubhouse.py
@@ -1,3 +1,23 @@
+# Copyright (C) 2018 Endless Mobile, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Authors:
+#       Joaquim Rocha <jrocha@endlessm.com>
+#
+
 import gi
 gi.require_version("Gdk", "3.0")
 gi.require_version("Gtk", "3.0")
@@ -10,35 +30,63 @@ CLUBHOUSE_NAME = 'com.endlessm.Clubhouse'
 CLUBHOUSE_PATH = '/com/endlessm/Clubhouse'
 CLUBHOUSE_IFACE = CLUBHOUSE_NAME
 
-ClubhouseIface = ('<node><interface name="com.endlessm.Clubhouse">'
+ClubhouseIface = ('<node>'
+                  '<interface name="com.endlessm.Clubhouse">'
                   '<method name="show">'
-                    '<arg type="u" direction="in" name="timestamp"/>'
+                  '<arg type="u" direction="in" name="timestamp"/>'
                   '</method>'
                   '<method name="hide">'
-                    '<arg type="u" direction="in" name="timestamp"/>'
+                  '<arg type="u" direction="in" name="timestamp"/>'
                   '</method>'
                   '<property name="Visible" type="b" access="read"/>'
-                  '</interface></node>')
+                  '</interface>'
+                  '</node>')
 
 
 class ClubhouseWindow(Gtk.ApplicationWindow):
+
+    DEFAULT_WINDOW_WIDTH = 500
 
     def __init__(self, app):
         super().__init__(application=app, title='Clubhouse',
                          type_hint=Gdk.WindowTypeHint.DOCK,
                          role='eos-side-component')
+        self.set_size_request(self.DEFAULT_WINDOW_WIDTH, -1)
+        self._setup_ui()
 
         self.get_style_context().add_class('main-window')
 
-class ClubhouseApplication(Gtk.Application):
+    def _setup_ui(self):
+        builder = Gtk.Builder()
+        builder.add_from_resource('/com/endlessm/Clubhouse/main-window.ui')
+        self._main_box = builder.get_object('main_box')
+        self.add(self._main_box)
 
+
+class ClubhouseApplication(Gtk.Application):
 
     def __init__(self):
         super().__init__(application_id=CLUBHOUSE_NAME,
                          flags=Gio.ApplicationFlags.FLAGS_NONE)
 
+        self._init_style()
+
         self._window = None
         self._dbus_connection = None
+
+    def _init_style(self):
+        self.props.resource_base_path = '/com/endlessm/Clubhouse'
+        # @todo: Move the resource to a different dir
+        resource = Gio.resource_load(os.path.join(os.path.dirname(__file__),
+                                                  'eos-clubhouse.gresource'))
+        Gio.Resource._register(resource)
+        css_file = Gio.File.new_for_uri('resource:///com/endlessm/Clubhouse/gtk-style.css')
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_file(css_file)
+        style_context = Gtk.StyleContext()
+        style_context.add_provider_for_screen(Gdk.Screen.get_default(),
+                                              css_provider,
+                                              Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
     def do_activate(self):
         pass
@@ -47,7 +95,7 @@ class ClubhouseApplication(Gtk.Application):
         Gtk.Application.do_startup(self)
 
         self._window = ClubhouseWindow(self)
-        self._window.connect('notify::visible', self._vibility_notify_cb);
+        self._window.connect('notify::visible', self._vibility_notify_cb)
 
         display = Gdk.Display.get_default()
         display.connect('monitor-added',
