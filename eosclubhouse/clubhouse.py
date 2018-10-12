e7cc006045c3481bc5a28118d947c5137144538e
Joaquim Rocha <jrocha@endlessm.com>
2018-10-12 20:40:50 +0200

    Don't hide the Clubhouse when running a quest
    The quests are supposed to be first shown in the Clubhouse main window,
    and the Shell qeust view is only supposed to be shown per user command
    (TBD).

--- eosclubhouse/clubhouse.py
+++ eosclubhouse/clubhouse.py
@@ -250,11 +250,6 @@ class ClubhouseWindow(Gtk.ApplicationWindow):
     def run_quest(self, quest):
         self.set_character(quest.get_main_character())
 
-        # @todo Only show the Shell quest view when inviting the character out, as
-        # per design direction. For now we just hide the main window.
-        # The quest will be shown in the Shell quest view
-        self.hide()
-
         logger.info('Running quest "%s"', quest)
 
         self.connect_quest(quest)
