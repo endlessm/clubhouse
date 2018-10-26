0a2084c5319c420f3735ede65ca91df5418a6a72
Joaquim Rocha <jrocha@endlessm.com>
2018-10-26 17:10:58 +0200

    utils: Abstract the QuestStringCatalog's base into its own class
    We will have more data described in CSV files (e.g. "DB" of items given
    by quests), so we should have a base class to easily implement these
    CSV->dict classes.

--- eosclubhouse/utils.py
+++ eosclubhouse/utils.py
@@ -31,36 +31,54 @@ def get_alternative_quests_dir():
     return os.path.join(GLib.get_user_data_dir(), 'quests')
 
 
-class QuestStringCatalog:
+class _DictFromCSV:
 
-    _string_dict = {}
+    _csv_dict = {}
 
-    def __init__(self):
-        if self._string_dict:
+    def __init__(self, csv_path):
+        if self._csv_dict:
             return
-        self._load_csv()
+        self.load_csv(csv_path)
 
     @classmethod
-    def _load_csv(class_):
-        strings = {}
+    def load_csv(class_, csv_original_path):
+        contents = {}
 
-        file_name = os.path.basename(config.QUESTS_STRINGS_CSV)
-        dirs = [config.QUESTS_STRINGS_CSV,
-                os.path.join(get_alternative_quests_dir(), file_name)]
+        file_name = os.path.basename(csv_original_path)
+        dirs = [csv_original_path, os.path.join(get_alternative_quests_dir(), file_name)]
 
         for csv_path in dirs:
             if not os.path.exists(csv_path):
                 continue
 
             with open(csv_path, 'r') as csv_file:
-                for key, value, _character in csv.reader(csv_file):
-                    strings[key] = value
+                for row in csv.reader(csv_file):
+                    class_.set_key_value_from_csv_row(row, contents)
 
-        class_._string_dict = strings
+        class_._csv_dict = contents
+
+    @classmethod
+    def get_dict(class_):
+        return class_._csv_dict
+
+    @classmethod
+    def set_key_value_from_csv_row(class_, csv_row, contents_dict):
+        raise NotImplementedError()
+
+
+class QuestStringCatalog(_DictFromCSV):
+
+    def __init__(self):
+        super().__init__(config.QUESTS_STRINGS_CSV)
 
     @classmethod
     def get_string(class_, key):
-        return class_._string_dict.get(key)
+        return class_.get_dict().get(key)
+
+    @classmethod
+    def set_key_value_from_csv_row(class_, csv_row, contents_dict):
+        key, value = csv_row[0], csv_row[1]
+        contents_dict[key] = value
 
 
 # Convenience "QuestString" method to get a string from the catalog
