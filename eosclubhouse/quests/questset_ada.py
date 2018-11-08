2d27a7b0e97f13cc2ec2efb487d7cabce44872a1
Noel Llopis <noel@endlessm.com>
2018-11-08 17:45:25 +0100

    Quests: Add LostFiles final quest
    To wrap up the first act and specific no-quest dialog once the act is
    over.

--- eosclubhouse/quests/questset_ada.py
+++ eosclubhouse/quests/questset_ada.py
@@ -5,19 +5,24 @@ from eosclubhouse.quests.firstcontact import FirstContact
 from eosclubhouse.quests.roster import Roster
 from eosclubhouse.quests.fizzicsintro import FizzicsIntro
 from eosclubhouse.quests.osintro import OSIntro
+from eosclubhouse.quests.lostfiles import LostFiles
 
 
 class AdaQuestSet(QuestSet):
 
     __character_id__ = 'ada'
     __position__ = (38, 208)
-    __quests__ = [FirstContact(), Roster(), FizzicsIntro(), OSIntro()]
+    __quests__ = [FirstContact(), Roster(), FizzicsIntro(), OSIntro(), LostFiles()]
 
     def get_empty_message(self):
         if Registry.get_quest_set_by_name('RickyQuestSet').is_active():
             return QS('NOQUEST_ADA_RICKY')
         if Registry.get_quest_set_by_name('ArchivistQuestSet').is_active():
             return QS('NOQUEST_ADA_ARCHIVER')
+
+        quest = self.get_quests()[0]
+        if (quest.is_named_quest_complete("LostFiles")):
+            return QS('NOQUEST_ADA_CHAPTER1END')
 
         return QS('NOQUEST_ADA_NOTHING')
 
--- eosclubhouse/quests/questset_archivist.py
+++ eosclubhouse/quests/questset_archivist.py
@@ -13,14 +13,18 @@ class ArchivistQuestSet(QuestSet):
     def __init__(self):
         super().__init__()
         first_quest = self.get_quests()[0]
-        self.visible = first_quest.available or first_quest.conf['complete'] or \
-                        first_quest.is_named_quest_complete('OSIntro')
+        self.visible = (first_quest.available or first_quest.conf['complete'] or
+                        first_quest.is_named_quest_complete('OSIntro'))
 
     def get_empty_message(self):
         if Registry.get_quest_set_by_name('AdaQuestSet').is_active():
             return QS('NOQUEST_ARCHIVIST_ADA')
         if Registry.get_quest_set_by_name('RickyQuestSet').is_active():
             return QS('NOQUEST_ARCHIVIST_RICKY')
+
+        quest = self.get_quests()[0]
+        if (quest.is_named_quest_complete("LostFiles")):
+            return QS('NOQUEST_ARCHIVIST_CHAPTER1END')
 
         return QS('NOQUEST_ARCHIVIST_NOTHING')
 
