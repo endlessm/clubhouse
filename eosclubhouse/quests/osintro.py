14e69eba10d9caa4188a37cc3eb3ef55d088cfe8
Noel Llopis <noel@endlessm.com>
2018-11-21 15:28:48 +0100

    quest: OS Intro improved timing between dialong and giving System app

--- eosclubhouse/quests/osintro.py
+++ eosclubhouse/quests/osintro.py
@@ -14,6 +14,13 @@ class OSIntro(Quest):
     # STEP 0
     def step_first(self, time_in_step):
         if time_in_step == 0:
+            self.show_question(QS('OSINTRO_PRELAUNCH'))
+
+        if self.confirmed_step():
+            return self.step_launch
+
+    def step_launch(self, time_in_step):
+        if time_in_step == 0:
             self.show_message(QS('OSINTRO_LAUNCH'))
             Sound.play('quests/new-icon')
             Desktop.add_app_to_grid(self.TARGET_APP_DBUS_NAME)
@@ -35,6 +42,9 @@ class OSIntro(Quest):
                 return self.step_archivist_flip
         except Exception as e:
             print(e)
+
+        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
+            return self.step_abort
 
     def step_archivist(self, time_in_step):
         if time_in_step == 0:
