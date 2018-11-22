ddbaeae4f14f9c39d836013d9a9c46cf3c9cec4d
Noel Llopis <noel@endlessm.com>
2018-11-22 19:06:50 +0100

    quest: Hints in most places that require action in BreakSomething

--- eosclubhouse/quests/breaksomething.py
+++ eosclubhouse/quests/breaksomething.py
@@ -22,64 +22,122 @@ class BreakSomething(Quest):
         if self.is_named_quest_complete("OSIntro"):
             self.available = True
 
+    def set_hints(self, dialog_id):
+        self._hintIndex = -1
+        self._hints = [QS(dialog_id)]
+        hintIndex = 0
+        while True:
+            hintIndex += 1
+            hintId = dialog_id + '_HINT' + str(hintIndex)
+            hintStr = QS(hintId)
+            if hintStr is None:
+                break
+            self._hints.append(hintStr)
+        self.show_hint()
+
+    def show_hint(self):
+        label = 'Hint'
+        if self._hintIndex >= len(self._hints) - 1:
+            self._hintIndex = 0
+        else:
+            self._hintIndex += 1
+            if self._hintIndex == len(self._hints) - 1:
+                label = 'Goal'
+        self.show_message(self._hints[self._hintIndex], choices=[(label, self.show_hint)])
+
     # STEP 0
     def step_first(self, time_in_step):
         if time_in_step == 0:
-            self.show_message(QS('BREAK_LAUNCH'))
+            self.set_hints('BREAK_LAUNCH')
             Desktop.show_app_grid()
 
         if Desktop.app_is_running(self.TARGET_APP_DBUS_NAME) or self.debug_skip():
             return self.step_delay1
 
     def step_delay1(self, time_in_step):
-        if time_in_step > 2:
+        if time_in_step > 1:
             return self.step_explanation
 
     def step_explanation(self, time_in_step):
         if time_in_step == 0:
-            self.show_message(QS('BREAK_OSAPP'))
+            self.show_question(QS('BREAK_OSAPP'))
+        if self.confirmed_step():
+            return self.step_flip
+        try:
+            if self._app.get_object_property('view.JSContext.globalParameters', 'flipped'):
+                return self.step_givekey
+        except Exception as e:
+            print(e)
+        # Abort if exiting app
+        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
+            return self.step_abort
+
+    def step_flip(self, time_in_step):
+        if time_in_step == 0:
+            self.set_hints('BREAK_FLIP')
 
         try:
             if self._app.get_object_property('view.JSContext.globalParameters', 'flipped'):
                 return self.step_givekey
         except Exception as e:
             print(e)
-
+        # Abort if exiting app
         if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
             return self.step_abort
 
     def step_givekey(self, time_in_step):
         if time_in_step == 0:
-            item = self.gss.get('item.key.OperatingSystemApp.1')
-            # If we already have the key, skip this step.
-            if item is not None:
-                # If the panel is already unlocked, skip all that
-                if item.get('used', False):
-                    return self.step_unlocked
-                # Otherwise prompt player to unlock it
-                else:
-                    return self.step_unlock
+            try:
+                item = self.gss.get('item.key.OperatingSystemApp.1')
+                # If we already have the key, skip this step.
+                if item is not None:
+                    # If the panel is already unlocked, skip all that
+                    if item.get('used', False):
+                        return self.step_unlocked
+                    # Otherwise prompt player to unlock it
+                    else:
+                        return self.step_unlock
+            except Exception as e:
+                print(e)
             self.show_question(QS('BREAK_GIVEKEY'))
+
         if self.confirmed_step():
             self.give_item('item.key.OperatingSystemApp.1')
             return self.step_unlock
+        # Abort if exiting app
+        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
+            return self.step_abort
 
     def step_unlock(self, time_in_step):
         if time_in_step == 0:
-            self.show_message(QS('BREAK_UNLOCK'))
+            self.set_hints('BREAK_UNLOCK')
 
         item = self.gss.get('item.key.OperatingSystemApp.1')
         if item is not None and item.get('used', False):
             return self.step_unlocked
+        # Abort if exiting app
         if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
             return self.step_abort
 
     def step_unlocked(self, time_in_step):
         if time_in_step == 0:
-            self.show_message(QS('BREAK_UNLOCKED'))
+            self.show_question(QS('BREAK_UNLOCKED'))
+
+        if self.confirmed_step():
+            return self.step_makeitlarge
+        if self.settings.get_int('cursor-size') >= 200:
+            return self.step_success
+        # Abort if exiting app
+        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
+            return self.step_abort
+
+    def step_makeitlarge(self, time_in_step):
+        if time_in_step == 0:
+            self.set_hints('BREAK_MAKEITLARGE')
 
         if self.settings.get_int('cursor-size') >= 200:
             return self.step_success
+        # Abort if exiting app
         if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
             return self.step_abort
 
