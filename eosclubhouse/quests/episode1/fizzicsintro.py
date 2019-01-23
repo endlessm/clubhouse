2c1b4283963a209d4e9c3883c829c96201096a14
Joaquim Rocha <jrocha@endlessm.com>
2019-01-23 22:25:49 +0100

    quests: Port FizzicsIntro quest to the non-polling approach
    https://phabricator.endlessm.com/T24699

--- eosclubhouse/quests/episode1/fizzicsintro.py
+++ eosclubhouse/quests/episode1/fizzicsintro.py
@@ -4,119 +4,106 @@ from eosclubhouse.system import Desktop, App, Sound
 
 class FizzicsIntro(Quest):
 
-    TARGET_APP_DBUS_NAME = 'com.endlessm.Fizzics'
+    APP_NAME = 'com.endlessm.Fizzics'
 
     def __init__(self):
         super().__init__('Fizzics Intro', 'ada')
-        self._app = App(self.TARGET_APP_DBUS_NAME)
+        self._app = App(self.APP_NAME)
+        self._level = -1
 
     def get_current_level(self):
+        if self.debug_skip():
+            self._level += 1
+            return self._level
+
+        level = -1
+        success = False
         try:
             level = self._app.get_js_property('currentLevel')
-            return level
+            success = self._app.get_js_property('levelSuccess')
         except Exception as ex:
             print(ex)
-        return -1
 
-    # STEP 0
-    def step_first(self, time_in_step):
-        if time_in_step == 0:
-            if Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
-                return self.step_explanation
-            self.show_hints_message('LAUNCH')
-            Sound.play('quests/new-icon')
-            Desktop.add_app_to_grid(self.TARGET_APP_DBUS_NAME)
-            Desktop.focus_app(self.TARGET_APP_DBUS_NAME)
+        self._level = level
 
-        if Desktop.app_is_running(self.TARGET_APP_DBUS_NAME) or self.debug_skip():
-            return self.step_delay_until_ready
+        if self._level != -1 and (success or self.debug_skip()):
+            self._level += 1
 
-    # Wait until the app is up and running
-    def step_delay_until_ready(self, time_in_step):
-        if self.get_current_level() >= 0:
-            return self.step_delay1
+        return self._level
 
-    # And now give it 1 more second to initialize its current level
-    # That way we can check at the beginning of the next step and not print multiple messages
-    def step_delay1(self, time_in_step):
-        if time_in_step >= 1:
-            return self.step_explanation
+    def step_begin(self):
+        self._level = -1
 
-    def step_explanation(self, time_in_step):
-        if time_in_step == 0:
-            Sound.play('quests/step-forward')
-            if self.get_current_level() >= 2:
-                return self.step_already_beat
-            self.show_question('EXPLANATION')
+        if self._app.is_running():
+            return self.step_launched
 
-        if self.confirmed_step():
-            return self.step_level1
-        if self.get_current_level() >= 1:
-            return self.step_level2
-        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
-            return self.step_abort
+        self.show_hints_message('LAUNCH')
 
-    def step_level1(self, time_in_step):
-        if time_in_step == 0:
+        Sound.play('quests/new-icon')
+        Desktop.add_app_to_grid(self.APP_NAME)
+        Desktop.focus_app(self.APP_NAME)
+
+        self.wait_for_app_launch(self._app)
+
+        # And delay the next step to let the game initialize its current level
+        self.set_next_step(self.step_launched, delay=1)
+
+    def step_abort(self):
+        Sound.play('quests/quest-aborted')
+        self.show_message('ABORT')
+
+        self.set_next_step(self.stop, delay=5)
+
+    @Quest.with_app_launched(APP_NAME, step_abort)
+    def step_launched(self):
+        self.set_next_step(self.step_explanation)
+
+    @Quest.with_app_launched(APP_NAME, step_abort)
+    def step_check_level(self):
+        level = self.get_current_level()
+
+        if level <= 1:
             self.show_hints_message('LEVEL1')
-
-        if self.get_current_level() >= 1:
-            return self.step_level2
-        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
-            return self.step_abort
-
-    def step_level2(self, time_in_step):
-        if time_in_step == 0:
+        elif level == 2:
             Sound.play('quests/step-forward')
             self.show_hints_message('LEVEL2')
-
-        current_level = self.get_current_level()
-        if current_level >= 2 or \
-           (current_level == 1 and self._app.get_js_property('levelSuccess')):
-            return self.step_success
-        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME):
-            return self.step_abort
-
-    def step_success(self, time_in_step):
-        if time_in_step == 0:
-            self.show_question('SUCCESS')
-        if self.confirmed_step():
+        else:
+            self.wait_confirm('SUCCESS')
             return self.step_key
 
-    def step_already_beat(self, time_in_step):
-        if time_in_step == 0:
-            self.show_question('ALREADYBEAT')
-        if self.confirmed_step():
-            return self.step_key
+        self.wait_for_app_js_props_changed(self._app, ['levelSuccess'])
+        return self.step_check_level
 
-    def step_key(self, time_in_step):
-        if time_in_step == 0:
-            self.give_item('item.key.fizzics.1')
-            self.show_question('KEYAFTER')
-        if self.confirmed_step():
-            return self.step_riley
+    @Quest.with_app_launched(APP_NAME, otherwise=step_abort)
+    def step_explanation(self):
+        Sound.play('quests/step-forward')
 
-    def step_riley(self, time_in_step):
-        if time_in_step == 0:
-            Sound.play('quests/riley-intro')
-            self.show_question('RILEY')
-        if self.confirmed_step():
-            return self.step_end
+        if self.get_current_level() > 2:
+            return self.step_already_beat
 
-    def step_end(self, time_in_step):
-        if time_in_step == 0:
-            self.conf['complete'] = True
-            self.available = False
-            self.show_message('END', choices=[('Bye', self._confirm_step)])
-            Sound.play('quests/quest-complete')
-        if self.confirmed_step():
-            self.stop()
+        self.wait_confirm('EXPLANATION')
+        return self.step_check_level
 
-    # STEP Abort
-    def step_abort(self, time_in_step):
-        if time_in_step == 0:
-            Sound.play('quests/quest-aborted')
-            self.show_message('ABORT')
+    def step_success(self):
+        self.wait_confirm('SUCCESS')
+        return self.step_key
 
-        if time_in_step > 5:
-            self.stop()
+    def step_already_beat(self):
+        self.wait_confirm('ALREADYBEAT')
+        return self.step_key
+
+    def step_key(self):
+        self.give_item('item.key.fizzics.1')
+        self.wait_confirm('KEYAFTER')
+        return self.step_riley
+
+    def step_riley(self):
+        Sound.play('quests/riley-intro')
+        self.wait_confirm('RILEY')
+        return self.step_end
+
+    def step_end(self):
+        self.conf['complete'] = True
+        self.available = False
+        self.show_message('END', choices=[('Bye', self.stop)])
+        Sound.play('quests/quest-complete')
