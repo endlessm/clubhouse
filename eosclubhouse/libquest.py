2f0b0914d771498c28a2f61644d091025952cfec
Joaquim Rocha <jrocha@endlessm.com>
2019-01-29 14:37:35 +0100

    libquest: Refactor wait_for_app_js_props_changed Quest method
    It now uses the connect_app_js_props_changes to get the AsyncAction
    object and uses it to block until the action is done.
    
    https://phabricator.endlessm.com/T25214

--- eosclubhouse/libquest.py
+++ eosclubhouse/libquest.py
@@ -572,27 +572,7 @@ class Quest(GObject.GObject):
         return async_action
 
     def wait_for_app_js_props_changed(self, app, props, timeout=None):
-        assert self._run_context is not None
-
-        async_action = self._run_context.new_async_action()
-
-        def _on_app_running_changed(app, async_action):
-
-            if not app.is_running() and not async_action.is_resolved():
-                async_action.resolve()
-
-        if async_action.is_cancelled():
-            return async_action
-
-        js_props_handler_id = app.connect_js_props_change(props, lambda: async_action.resolve())
-        running_handler_id = app.connect_running_change(_on_app_running_changed, app, async_action)
-
-        self._run_context.wait_for_action(async_action, timeout)
-
-        app.disconnect_js_props_change(js_props_handler_id)
-        app.disconnect_running_change(running_handler_id)
-
-        return async_action
+        return self.connect_app_js_props_changes(app, props).wait(timeout)
 
     def wait_for_app_in_foreground(self, app, timeout=None):
         assert self._run_context is not None
@@ -655,7 +635,7 @@ class Quest(GObject.GObject):
         return self._run_context.pause(secs)
 
     def wait_confirm(self, msg_id=None, timeout=None):
-        return self.show_confirm_msg(msg_id).wait(timeout)
+        return self.show_confirm_message(msg_id).wait(timeout)
 
     def show_confirm_message(self, msg_id):
         assert self._run_context is not None
