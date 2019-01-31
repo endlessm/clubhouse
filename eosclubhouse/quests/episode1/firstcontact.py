cec66d2b3b9df1490341ca4f6ffa8badd2369a13
Manuel Quiñones <manuel.por.aca@gmail.com>
2019-01-31 16:32:51 -0300

    quests: Port FirstContact to the non-polling approach
    Note: this changes the behavior of the hint messages by using the same
    method as other quests to display them. This needs a change in the
    query strings. The next commit will import it from the spreadsheet.
    
    Also handle if the app stops running for any reason.
    
    https://phabricator.endlessm.com/T25214

--- eosclubhouse/quests/episode1/firstcontact.py
+++ eosclubhouse/quests/episode1/firstcontact.py
@@ -1,85 +1,69 @@
 from eosclubhouse.libquest import Quest
-from eosclubhouse.system import Desktop, App
+from eosclubhouse.system import App
 
 
 class FirstContact(Quest):
 
-    TARGET_APP_DBUS_NAME = 'com.endlessm.HackUnlock'
+    APP_NAME = 'com.endlessm.HackUnlock'
 
     # This quest starts already in the first step. There's no prompting.
     def __init__(self):
         super().__init__('First Contact', 'ada', '')
-        self._app = None
-        self._hint1 = False
-        self._hint2 = False
+        self._app = App(self.APP_NAME)
 
         # This will prevent the quest from ever being shown in the Clubhouse
         # because it should just be run directly (not by the user)
         self.available = False
         self.skippable = True
 
-    def get_hackunlock_mode(self):
-        if self._app is None:
-            self._app = App(self.TARGET_APP_DBUS_NAME)
+    def _is_app_flipped(self):
+        return self._app.get_js_property('mode', default_value=0) >= 1
 
-        mode = 0
+    def _is_app_hacked(self):
+        return self._app.get_js_property('mode', default_value=0) >= 2
 
-        try:
-            mode = self._app.get_object_property('view.JSContext.globalParameters', 'mode')
-        except Exception as e:
-            print(e)
+    def _is_app_flipped_back(self):
+        return self._app.get_js_property('mode', default_value=0) >= 4
 
-        return mode
-
-    # Waiting for player to flip app
-    def step_first(self, time_in_step):
-        # Starting out without any dialog
-
-        if not Desktop.app_is_running(self.TARGET_APP_DBUS_NAME) or time_in_step < 3:
-            return
-
-        if self.get_hackunlock_mode() >= 1:
-            self._hint1 = False
-            self._hint2 = False
-            return self.step_dohack
-
-        if time_in_step >= 10 and not self._hint1:
-            self.show_message('FIRSTCONTACT_WELCOME_HINT1')
-            self._hint1 = True
-        elif time_in_step >= 20 and not self._hint2:
-            self.show_message('FIRSTCONTACT_WELCOME_HINT2')
-            self._hint2 = True
-
-    # App has been flipped
-    def step_dohack(self, time_in_step):
-        if time_in_step == 0:
-            self.show_message('FIRSTCONTACT_GOAL')
-
-        if self.get_hackunlock_mode() >= 2:
-            self._hint1 = False
-            self._hint2 = False
-            return self.step_flipback
-
-        if time_in_step > 20 and not self._hint1:
-            self._hint1 = True
-            self.show_message('FIRSTCONTACT_GOAL_HINT1')
-        if time_in_step > 40 and not self._hint2:
-            self._hint2 = True
-            self.show_message('FIRSTCONTACT_GOAL_HINT2')
-
-    # Hack is done. Waiting for player to flip back
-    def step_flipback(self, time_in_step):
-        if time_in_step == 0:
-            self.show_message('FIRSTCONTACT_FLIPBACK')
-
-        if self.get_hackunlock_mode() >= 4 or self.debug_skip():
-            return self.step_reward
-
-        if time_in_step > 8 and not self._hint1:
-            self._hint1 = True
-            self.show_message('FIRSTCONTACT_FLIPBACK_HINT1')
-
-    def step_reward(self, time_in_step):
+    def step_reward(self):
         self.conf['complete'] = True
         self.available = False
         self.stop()
+
+    def step_begin(self):
+        self.wait_for_app_launch(self._app)
+        self.pause(3)
+        return self.step_one
+
+    @Quest.with_app_launched(APP_NAME, otherwise=step_reward)
+    def step_one(self):
+        if self._is_app_flipped():
+            return self.step_dohack
+
+        self.show_hints_message('WELCOME')
+        while not self._is_app_flipped():
+            self.wait_for_app_js_props_changed(self._app, ['mode'])
+
+        return self.step_dohack
+
+    @Quest.with_app_launched(APP_NAME, otherwise=step_reward)
+    def step_dohack(self):
+        if self._is_app_hacked():
+            return self.step_flipback
+
+        self.show_hints_message('GOAL')
+        while not self._is_app_hacked():
+            self.wait_for_app_js_props_changed(self._app, ['mode'])
+
+        return self.step_flipback
+
+    @Quest.with_app_launched(APP_NAME, otherwise=step_reward)
+    def step_flipback(self):
+        if self._is_app_flipped_back():
+            return self.step_reward
+
+        self.show_hints_message('FLIPBACK')
+        while not self._is_app_flipped_back():
+            self.wait_for_app_js_props_changed(self._app, ['mode'])
+
+        return self.step_reward
