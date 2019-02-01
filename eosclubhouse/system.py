811c2f418d94da6db2a4d9931ec3b22e8dd79517
Joaquim Rocha <jrocha@endlessm.com>
2019-02-01 17:49:10 +0100

    system: Change how the App notifies of its properties changes
    Due to a bug in Clippy, it may continuously notify of property changes
    even if the value didn't actually change. It was not noticeable for
    Clubhouse users due to how the quests were implemented, but with the new
    (non-polling) approach, the issue is exposed, and even though it is
    fixed in Clippy already, some users may still experiment this problem
    until they upgrade.
    
    Thus, this patch fixes that issue in the App class by checking if its
    properties' values have actually changed before calling the connected
    callback.
    
    https://phabricator.endlessm.com/T25276

--- eosclubhouse/system.py
+++ eosclubhouse/system.py
@@ -267,14 +267,24 @@ class App:
         return self.set_object_property(self.APP_JS_PARAMS, prop, value)
 
     def connect_js_props_change(self, props, js_property_changed_cb, *args):
+        # Check if the properties really changed, because in older versions of
+        # Clippy, it was notifying always, instead of only if the value of the
+        # property had changed.
+        # @todo: Remove once it's safe for our users to use this logic without this
+        # safeguard.
+        values = {}
+        for prop in props:
+            values[prop] = self.get_js_property(prop)
+
         def _props_changed_cb(_proxy, _owner, signal_name, params, props, js_property_changed_cb,
                               *args):
             if signal_name != 'ObjectNotify':
                 return
 
-            _notify_obj, notify_prop, _value = params.unpack()
+            _notify_obj, notify_prop, value = params.unpack()
 
-            if notify_prop in props:
+            if notify_prop in props and value != values[notify_prop]:
+                values[notify_prop] = value
                 js_property_changed_cb(*args)
 
         for prop in props:
