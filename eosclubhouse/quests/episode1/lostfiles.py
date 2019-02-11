28573f4640e599174de9c0da48d823b58f168288
Joaquim Rocha <jrocha@endlessm.com>
2019-02-11 13:37:32 +0100

    quests: Only set Episode 1 as complete after user confirmation
    The LostFiles quest shows an "End of Episode 1" button to the user,
    so the episode (and the quest) should only be set as such after the
    user clicks that button. Otherwise the poster window is shown before
    the button is clicked.
    
    https://phabricator.endlessm.com/T25123

--- eosclubhouse/quests/episode1/lostfiles.py
+++ eosclubhouse/quests/episode1/lostfiles.py
@@ -23,9 +23,10 @@ class LostFiles(Quest):
         self.wait_confirm('EXPLANATION2')
         self.wait_confirm('EXPLANATION3')
 
-        self.conf['complete'] = True
-        self.available = False
-        self.complete_current_episode()
-        Sound.play('quests/quest-complete')
+        self.show_message('EXPLANATION4', choices=[('End of Episode 1', self.finish_episode)])
 
-        self.show_message('EXPLANATION4', choices=[('End of Episode 1', self.stop)])
+    def finish_episode(self):
+        Sound.play('quests/quest-complete')
+        self.conf['complete'] = True
+        self.complete_current_episode()
+        self.stop()
