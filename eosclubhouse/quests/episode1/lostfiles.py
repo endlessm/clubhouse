endlessm/clubhouse

commit 973ff275528894d93ded0a5ad717b11d260b2b01

quests: Only set Episode 1 as complete after user confirmation
Port of commit 28573f46 for stable.

https://phabricator.endlessm.com/T25123

eosclubhouse/quests/episode1/lostfiles.py
@@ -38,11 +38,15 @@ class LostFiles(Quest):
 
     def step_explanation4(self, time_in_step):
         if time_in_step == 0:
+            self.show_message('EXPLANATION4',
+                              choices=[('End of Episode 1', self._confirm_step)])
+        if self.confirmed_step():
+            self.step_finish_episode()
+
+    def step_finish_episode(self, time_in_step):
+        if time_in_step == 0:
             self.conf['complete'] = True
             self.available = False
             self.complete_current_episode()
             Sound.play('quests/quest-complete')
-            self.show_message('EXPLANATION4',
-                              choices=[('End of Episode 1', self._confirm_step)])
-        if self.confirmed_step():
             self.stop()

commit b95e39fcc2181fd4f60c4918d4c331039b05896f

quests: Yet another fix for rebasing LostFiles into stable
Fixes a bug introduced in 973ff275 .

https://phabricator.endlessm.com/T25405

eosclubhouse/quests/episode1/lostfiles.py
@@ -41,7 +41,7 @@ class LostFiles(Quest):
             self.show_message('EXPLANATION4',
                               choices=[('End of Episode 1', self._confirm_step)])
         if self.confirmed_step():
-            self.step_finish_episode()
+            self.step_finish_episode
 
     def step_finish_episode(self, time_in_step):
         if time_in_step == 0: