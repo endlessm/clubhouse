8bb4c63baab3e3c50f8c39f7952ab5397bd0ca6a
Joaquim Rocha <jrocha@endlessm.com>
2019-02-11 18:31:12 +0100

    Revert changes to LostFiles quest finish step
    The changes had been adapted as if the LostFiles quest had been ported to
    the non-polling approach, but this is not the case in the stable branch,
    so those changes need to be fully adapted.
    
    Thus, this patch reverts the following commits:
      "quests: Make LostFiles unavailable after it's complete"
      5129a9b7c285019fff7baa605d69862f9825e2cb
    
      "quests: Only set Episode 1 as complete after user confirmation"
      5664ccbe1933ee1901424a7ad79554e4e1349cdf
    
      "quests: Complete Episode when finishing LostFiles quest instead"
      506c9b82d4cccfad9d1de1fe8e46751bc3f2c0ff

--- eosclubhouse/quests/episode1/fizzicscode2.py
+++ eosclubhouse/quests/episode1/fizzicscode2.py
@@ -69,6 +69,7 @@ class FizzicsCode2(Quest):
             Sound.play('quests/step-forward')
             self.conf['complete'] = True
             self.available = False
+            self.complete_current_episode()
             Sound.play('quests/quest-complete')
             self.show_message('END', choices=[('Bye', self._confirm_step)])
         if self.confirmed_step():
