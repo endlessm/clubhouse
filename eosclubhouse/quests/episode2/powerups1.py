a1842076e678f7b97fb4b6486802bf5d31f950e8
Joaquim Rocha <jrocha@endlessm.com>
2019-02-18 20:30:55 +0100

    quests: Port PowerUps1 quest to the non-polling approach
    This quest didn't check any properties in the app (it's simply a
    skeleton waiting for the user debug action to occur), so I have made
    this new version wait for a dummy property on the app. This way we
    have the same effect as before but it's easier to spot which methods
    need to be changed (as the errors are shown on the log).
    
    https://phabricator.endlessm.com/T25404

--- eosclubhouse/quests/episode2/powerups1.py
+++ eosclubhouse/quests/episode2/powerups1.py
@@ -1,6 +1,6 @@
 from eosclubhouse.apps import LightSpeed
 from eosclubhouse.libquest import Quest
-from eosclubhouse.system import Desktop, Sound
+from eosclubhouse.system import Sound
 
 
 class PowerUps1(Quest):
@@ -11,74 +11,57 @@ class PowerUps1(Quest):
         super().__init__('PowerUps1', 'ada')
         self._app = LightSpeed()
 
-    def step_first(self, time_in_step):
-        if time_in_step == 0:
-            if Desktop.app_is_running(self.APP_NAME):
-                return self.step_explanation
+    def step_begin(self):
+        if not self._app.is_running():
             self.show_hints_message('LAUNCH')
             self.give_app_icon(self.APP_NAME)
+            self.wait_for_app_launch(self._app, pause_after_launch=2)
 
-        if Desktop.app_is_running(self.APP_NAME) or self.debug_skip():
-            return self.step_delay
+        self.show_hints_message('EXPLAIN')
+        return self.step_codepowerup
 
-    def step_delay(self, time_in_step):
-        if time_in_step >= 2:
-            return self.step_explanation
+    def step_abort(self):
+        Sound.play('quests/quest-aborted')
+        self.show_message('ABORT')
 
-    def step_explanation(self, time_in_step):
-        if time_in_step == 0:
-            self.show_hints_message('EXPLAIN')
+        self.pause(5)
+        self.stop()
 
-        if self.debug_skip():
-            return self.step_codepowerup
+    @Quest.with_app_launched(APP_NAME, otherwise=step_abort)
+    def step_codepowerup(self):
+        if not self._app.get_js_property('flipped') or self.debug_skip():
+            self.wait_for_app_js_props_changed(self._app, ['flipped'])
 
-        if not Desktop.app_is_running(self.APP_NAME):
-            return self.step_abort
+        self.show_hints_message('CODEPOWERUP')
 
-    def step_codepowerup(self, time_in_step):
-        if time_in_step == 0:
-            self.show_hints_message('CODEPOWERUP')
+        while not (self.debug_skip() or self.is_cancelled()):
+            self.wait_for_app_js_props_changed(self._app, ['DummyProperty'])
 
-        if self.debug_skip():
-            return self.step_spawn
+        return self.step_spawn
 
-        if not Desktop.app_is_running(self.APP_NAME):
-            return self.step_abort
+    @Quest.with_app_launched(APP_NAME, otherwise=step_abort)
+    def step_spawn(self):
+        self.show_hints_message('SPAWN')
 
-    def step_spawn(self, time_in_step):
-        if time_in_step == 0:
-            self.show_hints_message('SPAWN')
+        while not (self.debug_skip() or self.is_cancelled()):
+            self.wait_for_app_js_props_changed(self._app, ['DummyProperty'])
 
-        if self.debug_skip():
-            return self.step_success
+        return self.step_success
 
-        if not Desktop.app_is_running(self.APP_NAME):
-            return self.step_abort
+    @Quest.with_app_launched(APP_NAME, otherwise=step_abort)
+    def step_success(self):
+        self.show_hints_message('SUCCESS')
 
-    def step_success(self, time_in_step):
-        if time_in_step == 0:
-            self.show_hints_message('SUCCESS')
+        while not (self.debug_skip() or self.is_cancelled()):
+            self.wait_for_app_js_props_changed(self._app, ['DummyProperty'])
 
-        if self.debug_skip():
-            return self.step_end
+        return self.step_end
 
-        if not Desktop.app_is_running(self.APP_NAME):
-            return self.step_abort
+    def step_end(self):
+        self.conf['complete'] = True
+        self.available = False
 
-    def step_end(self, time_in_step):
-        if time_in_step == 0:
-            self.conf['complete'] = True
-            self.available = False
-            self.show_question('END', confirm_label='Bye')
-            Sound.play('quests/quest-complete')
+        Sound.play('quests/quest-complete')
+        self.show_confirm_message('END', confirm_label='Bye').wait()
 
-        if self.confirmed_step():
-            self.stop()
-
-    def step_abort(self, time_in_step):
-        if time_in_step == 0:
-            Sound.play('quests/quest-aborted')
-            self.show_message('ABORT')
-
-        if time_in_step > 5:
-            self.stop()
+        self.stop()
