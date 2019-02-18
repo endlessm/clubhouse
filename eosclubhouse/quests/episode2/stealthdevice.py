13dbe3e3d843f8fedaf9511dd542d7de88b31877
Joaquim Rocha <jrocha@endlessm.com>
2019-02-18 21:18:00 +0100

    quests: Port the StealthDevice quest to the non-polling approach
    https://phabricator.endlessm.com/T25404

--- eosclubhouse/quests/episode2/stealthdevice.py
+++ eosclubhouse/quests/episode2/stealthdevice.py
@@ -6,8 +6,8 @@ class StealthDevice(Quest):
 
     def __init__(self):
         super().__init__('StealthDevice', 'faber')
-        self.gss.connect('changed', self.update_availability)
         self.available = False
+        self.gss.connect('changed', self.update_availability)
         self.update_availability()
 
     def update_availability(self, gss=None):
@@ -16,35 +16,18 @@ class StealthDevice(Quest):
         if self.is_named_quest_complete("Hackdex2Decrypt"):
             self.available = True
 
-    def step_first(self, time_in_step):
-        if time_in_step == 0:
-            self.show_question('RILEYQUESTION')
-        if self.confirmed_step():
-            return self.step_explanation
+    def step_begin(self):
+        self.wait_confirm('RILEYQUESTION')
+        self.wait_confirm('EXPLANATION')
+        self.wait_confirm('RILEYTROUBLE')
+        self.wait_confirm('EXPLANATION2')
+        return self.step_end
 
-    def step_explanation(self, time_in_step):
-        if time_in_step == 0:
-            self.show_question('EXPLANATION')
-        if self.confirmed_step():
-            return self.step_rileytrouble
+    def step_end(self):
+        self.conf['complete'] = True
+        self.available = False
 
-    def step_rileytrouble(self, time_in_step):
-        if time_in_step == 0:
-            self.show_question('RILEYTROUBLE')
-        if self.confirmed_step():
-            return self.step_explanation2
+        Sound.play('quests/quest-complete')
+        self.show_confirm_message('END', confirm_label='Bye').wait()
 
-    def step_explanation2(self, time_in_step):
-        if time_in_step == 0:
-            self.show_question('EXPLANATION2')
-        if self.confirmed_step():
-            return self.step_end
-
-    def step_end(self, time_in_step):
-        if time_in_step == 0:
-            self.conf['complete'] = True
-            self.available = False
-            self.show_question('END', confirm_label='Bye')
-            Sound.play('quests/quest-complete')
-        if self.confirmed_step():
-            self.stop()
+        self.stop()
