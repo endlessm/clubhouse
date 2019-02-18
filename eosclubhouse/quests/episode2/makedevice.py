027a81ad63d216be31ef30542fa90f2220bfb537
Joaquim Rocha <jrocha@endlessm.com>
2019-02-18 22:11:56 +0100

    quests: Port the MakerDevice quest to the non-polling approach
    The quest does not do much except for showing messages, so all the
    methods could be condensed into one.
    
    https://phabricator.endlessm.com/T25404

--- eosclubhouse/quests/episode2/makedevice.py
+++ eosclubhouse/quests/episode2/makedevice.py
@@ -7,48 +7,36 @@ class MakeDevice(Quest):
     def __init__(self):
         super().__init__('MakeDevice', 'faber')
 
-    def have_all_stealth_items(self):
-        return (self.gss.get('item.stealth.1') is not None and
-                self.gss.get('item.stealth.2') is not None and
-                self.gss.get('item.stealth.3') is not None and
-                self.gss.get('item.stealth.4') is not None)
+    def has_all_stealth_items(self):
+        items = ['item.stealth.1', 'item.stealth.2', 'item.stealth.3', 'item.stealth.4']
+        for item in items:
+            if self.gss.get('item.stealth.1') is None:
+                return False
+        return True
 
-    def step_first(self, time_in_step):
-        if time_in_step == 0:
-            if not self.have_all_stealth_items():
-                self.stop()
-                return
-            self.show_question('EXPLAIN')
+    def step_begin(self):
+        if not self.has_all_stealth_items():
+            return self.step_missing_items
 
-        if self.confirmed_step():
-            return self.step_give_item
+        self.wait_confirm('EXPLAIN')
 
-    def step_give_item(self, time_in_step):
-        if time_in_step == 0:
-            self.show_question('GIVEITEM')
+        if not self.wait_confirm('GIVEITEM').is_cancelled():
             self.give_item('item.stealth_device')
-        if self.confirmed_step():
-            return self.step_riley
 
-    def step_riley(self, time_in_step):
-        if time_in_step == 0:
-            self.show_question('RILEY')
-        if self.confirmed_step():
-            return self.step_end
+        self.wait_confirm('RILEY')
 
-    def step_end(self, time_in_step):
-        if time_in_step == 0:
-            self.conf['complete'] = True
-            self.available = False
-            self.show_question('END', confirm_label='Bye')
-            Sound.play('quests/quest-complete')
-        if self.confirmed_step():
-            self.stop()
+        return self.step_end
 
-    def step_abort(self, time_in_step):
-        if time_in_step == 0:
-            Sound.play('quests/quest-aborted')
-            self.show_message('ABORT')
+    def step_missing_items(self):
+        # @todo: We should show some message here if some items are missing, otherwise
+        # the quest ends abruptely.
+        self.stop()
 
-        if time_in_step > 5:
-            self.stop()
+    def step_end(self):
+        self.conf['complete'] = True
+        self.available = False
+
+        Sound.play('quests/quest-complete')
+        self.show_confirm_message('END', confirm_label='Bye').wait()
+
+        self.stop()
