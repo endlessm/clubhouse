0696afee47768e7e8a0e2128f4b4f88a2aa83458
Joaquim Rocha <jrocha@endlessm.com>
2019-02-18 21:24:41 +0100

    quests: Port the MakerQuest quest to the non-polling approach
    https://phabricator.endlessm.com/T25404

--- eosclubhouse/quests/episode2/makerquest.py
+++ eosclubhouse/quests/episode2/makerquest.py
@@ -1,5 +1,6 @@
+from eosclubhouse.apps import LightSpeed
 from eosclubhouse.libquest import Quest
-from eosclubhouse.system import Desktop, App, Sound
+from eosclubhouse.system import Sound
 
 
 class MakerQuest(Quest):
@@ -8,36 +9,23 @@ class MakerQuest(Quest):
 
     def __init__(self):
         super().__init__('MakerQuest', 'faber')
-        self._app = App(self.APP_NAME)
+        self._app = LightSpeed()
 
-    def step_first(self, time_in_step):
-        if time_in_step == 0:
-            if Desktop.app_is_running(self.APP_NAME):
-                return self.step_end
+    def step_begin(self):
+        if not self._app.is_running():
             self.show_hints_message('LAUNCH')
             self.give_app_icon(self.APP_NAME)
+            self.wait_for_app_launch(self._app, pause_after_launch=2)
 
-        if Desktop.app_is_running(self.APP_NAME) or self.debug_skip():
-            return self.step_delay
+        return self.step_end
 
-    def step_delay(self, time_in_step):
-        if time_in_step >= 2:
-            return self.step_end
+    def step_end(self):
+        self.give_item('item.stealth.3')
 
-    def step_end(self, time_in_step):
-        if time_in_step == 0:
-            self.give_item('item.stealth.3')
-            self.conf['complete'] = True
-            self.available = False
-            self.show_question('END', confirm_label='Bye')
-            Sound.play('quests/quest-complete')
-        if self.confirmed_step():
-            self.stop()
+        self.conf['complete'] = True
+        self.available = False
 
-    def step_abort(self, time_in_step):
-        if time_in_step == 0:
-            Sound.play('quests/quest-aborted')
-            self.show_message('ABORT')
+        Sound.play('quests/quest-complete')
+        self.show_confirm_message('END', confirm_label='Bye').wait()
 
-        if time_in_step > 5:
-            self.stop()
+        self.stop()
