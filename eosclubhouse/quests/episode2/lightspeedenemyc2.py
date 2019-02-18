9ae8ca703678dca44c8436c5ae7c1f2c2bf951f7
Joaquim Rocha <jrocha@endlessm.com>
2019-02-18 22:03:23 +0100

    quests: Port the LightSpeedEnemyC2 quest to the non-polling approach
    https://phabricator.endlessm.com/T25404

--- eosclubhouse/quests/episode2/lightspeedenemyc2.py
+++ eosclubhouse/quests/episode2/lightspeedenemyc2.py
@@ -1,5 +1,6 @@
+from eosclubhouse.apps import LightSpeed
 from eosclubhouse.libquest import Quest
-from eosclubhouse.system import Desktop, App, Sound
+from eosclubhouse.system import Sound
 
 
 class LightSpeedEnemyC2(Quest):
@@ -8,47 +9,46 @@ class LightSpeedEnemyC2(Quest):
 
     def __init__(self):
         super().__init__('LightSpeedEnemyC2', 'riley')
-        self._app = App(self.APP_NAME)
+        self._app = LightSpeed()
 
-    def step_first(self, time_in_step):
-        if time_in_step == 0:
-            if Desktop.app_is_running(self.APP_NAME):
-                return self.step_explanation
+    def step_begin(self):
+        if not self._app.is_running():
             self.show_hints_message('LAUNCH')
             self.give_app_icon(self.APP_NAME)
+            self.wait_for_app_launch(self._app, pause_after_launch=2)
 
-        if Desktop.app_is_running(self.APP_NAME) or self.debug_skip():
-            return self.step_delay
+        return self.step_explanation
 
-    def step_delay(self, time_in_step):
-        if time_in_step >= 2:
-            return self.step_explanation
+    def step_abort(self):
+        Sound.play('quests/quest-aborted')
+        self.show_message('ABORT')
 
-    def step_explanation(self, time_in_step):
-        if time_in_step == 0:
-            self.show_hints_message('EXPLAIN')
-        if self.debug_skip():
-            return self.step_code
+        self.pause(5)
+        self.stop()
 
-    def step_code(self, time_in_step):
-        if time_in_step == 0:
-            self.show_hints_message('CODE')
-        if self.debug_skip():
-            return self.step_success
+    @Quest.with_app_launched(APP_NAME, otherwise=step_abort)
+    def step_explanation(self):
+        self.show_hints_message('EXPLAIN')
 
-    def step_success(self, time_in_step):
-        if time_in_step == 0:
-            self.conf['complete'] = True
-            self.available = False
-            self.show_question('SUCCESS', confirm_label='Bye')
-            Sound.play('quests/quest-complete')
-        if self.confirmed_step():
-            self.stop()
+        while not (self.debug_skip() or self.is_cancelled()):
+            self.wait_for_app_js_props_changed(self._app, ['DummyProperty'])
 
-    def step_abort(self, time_in_step):
-        if time_in_step == 0:
-            Sound.play('quests/quest-aborted')
-            self.show_message('ABORT')
+        return self.step_code
 
-        if time_in_step > 5:
-            self.stop()
+    @Quest.with_app_launched(APP_NAME, otherwise=step_abort)
+    def step_code(self):
+        self.show_hints_message('CODE')
+
+        while not (self.debug_skip() or self.is_cancelled()):
+            self.wait_for_app_js_props_changed(self._app, ['DummyProperty'])
+
+        return self.step_success
+
+    def step_success(self):
+        self.conf['complete'] = True
+        self.available = False
+
+        Sound.play('quests/quest-complete')
+        self.show_confirm_message('SUCCESS', confirm_label='Bye').wait()
+
+        self.stop()
