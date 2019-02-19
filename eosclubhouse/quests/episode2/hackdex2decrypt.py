e909495c419e88a855cbafb7d969b7feabc5d41b
Joaquim Rocha <jrocha@endlessm.com>
2019-02-19 10:35:36 +0100

    quests: Port the Hackdex2Decrypt quest to the non-polling approach
    Due to this quest using an app that doesn't exist yet, these changes have
    to call pause instead of waiting on a phony property as we did in other
    cases.
    
    https://phabricator.endlessm.com/T25404

--- eosclubhouse/quests/episode2/hackdex2decrypt.py
+++ eosclubhouse/quests/episode2/hackdex2decrypt.py
@@ -1,5 +1,5 @@
 from eosclubhouse.libquest import Quest
-from eosclubhouse.system import Desktop, App, Sound
+from eosclubhouse.system import App, Sound
 
 
 class Hackdex2Decrypt(Quest):
@@ -10,47 +10,43 @@ class Hackdex2Decrypt(Quest):
         super().__init__('Hackdex2Decrypt', 'riley')
         self._app = App(self.APP_NAME)
 
-    def step_first(self, time_in_step):
-        if time_in_step == 0:
-            if Desktop.app_is_running(self.APP_NAME):
-                return self.step_explanation
+    def step_begin(self):
+        if not self._app.is_running():
             self.show_hints_message('LAUNCH')
             self.give_app_icon(self.APP_NAME)
+            self.wait_for_app_launch(self._app, pause_after_launch=2)
 
-        if Desktop.app_is_running(self.APP_NAME) or self.debug_skip():
-            return self.step_delay
+        return self.step_explanation
 
-    def step_delay(self, time_in_step):
-        if time_in_step >= 2:
-            return self.step_explanation
+    # @todo: Set as needing the app to be running (we cannot add it yet since the app doesn't exist)
+    def step_explanation(self):
+        self.show_hints_message('EXPLANATION')
 
-    def step_explanation(self, time_in_step):
-        if time_in_step == 0:
-            self.show_hints_message('EXPLANATION')
-        if self.debug_skip():
-            return self.step_hack
+        # @todo: Check for FtH (the app used here doesn't exist so we cannot even wait for
+        # phony properties on it, and thus pause instead just for the debug).
+        while not (self.debug_skip() or self.is_cancelled()):
+            self.pause(3600)
 
-    def step_hack(self, time_in_step):
-        if time_in_step == 0:
-            self.show_hints_message('HACK')
-        if self.debug_skip():
-            return self.step_success
+        return self.step_hack
 
-    def step_success(self, time_in_step):
-        if time_in_step == 0:
-            self.conf['complete'] = True
-            self.available = False
-            self.show_question('SUCCESS', confirm_label='Bye')
+    # @todo: Set as needing the app to be running (we cannot add it yet since the app doesn't exist)
+    def step_hack(self):
+        self.show_hints_message('HACK')
+
+        # @todo: The app used here doesn't exist so we cannot even wait for phony properties on it,
+        # and thus pause instead just for the debug.
+        while not (self.debug_skip() or self.is_cancelled()):
+            self.pause(3600)
+
+        return self.step_success
+
+    def step_success(self):
+        self.conf['complete'] = True
+        self.available = False
+
+        self.show_confirm_message('SUCCESS', confirm_label='Bye').wait()
+
         if self.confirmed_step():
-            self.conf['complete'] = True
-            self.available = False
             Sound.play('quests/quest-complete')
-            self.stop()
 
-    def step_abort(self, time_in_step):
-        if time_in_step == 0:
-            Sound.play('quests/quest-aborted')
-            self.show_message('ABORT')
-
-        if time_in_step > 5:
-            self.stop()
+        self.stop()
