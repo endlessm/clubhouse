c55e9f237f8e7ba73d85fcbc92ac61ca96d7beef
Joaquim Rocha <jrocha@endlessm.com>
2019-03-06 14:58:26 +0100

    quests: Add page change detection to the Intro quest
    The Intro quest of the Episose 2 expects the user to access the
    episodes' page, so this patch adds that detection.
    
    https://phabricator.endlessm.com/T25578

--- eosclubhouse/quests/episode2/intro.py
+++ eosclubhouse/quests/episode2/intro.py
@@ -8,8 +8,15 @@ class Intro(Quest):
         super().__init__('Intro', 'riley')
 
     def step_begin(self):
-        self.show_confirm_message('EXPLANATION').wait()
-        # TODO: Wait for clicking on the Episodes tab on the Clubhouse
+        self.show_message('EXPLANATION')
+
+        while (self.clubhouse_state.current_page != self.clubhouse_state.Page.EPISODES and
+               not self.is_cancelled()):
+            self.connect_clubhouse_changes(['current-page']).wait()
+
+        return self.step_success
+
+    def step_success(self):
         self.show_confirm_message('EPISODESTAB', confirm_label='Bye').wait()
 
         self.conf['complete'] = True
