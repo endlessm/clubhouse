9a435e771448fceda1585628e3aaa4b54e7436a4
Joaquim Rocha <jrocha@endlessm.com>
2019-04-01 13:45:21 +0200

    clubhouse: Don't offer to continue quests that are stopping
    If a quest has declared it is stopping soon (through the new "stopping"
    property), then the Clubhouse shouldn't ask users if they want to
    continue the quest (when clicking in the quest's character).
    Instead, the normal "click" response will occur, as if the quest wasn't
    running.
    
    This prevents asking the user to continue a quest that will likely be
    stopped in a few seconds or less, which may cause confusion and a bad
    UX.
    
    https://phabricator.endlessm.com/T25931

--- eosclubhouse/clubhouse.py
+++ eosclubhouse/clubhouse.py
@@ -439,7 +439,8 @@ class ClubhousePage(Gtk.EventBox):
         # If a quest from this quest_set is already running, then just hide the window so the
         # user focuses on the Shell's quest dialog
         if self._current_quest:
-            if self._current_quest.available and self._current_quest in quest_set.get_quests():
+            if self._current_quest.available and self._current_quest in quest_set.get_quests() and \
+               not self._current_quest.stopping:
                 self._show_quest_continue_confirmation()
                 return
 
