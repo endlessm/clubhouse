7bbeeebeecbd3d58f3ae69fe4bc67994ad94de02
Manuel Quiñones <manuel.por.aca@gmail.com>
2019-04-08 10:43:44 -0300

    quests: Hook Fizzics challenges for episode 3
    https://phabricator.endlessm.com/T26031

--- eosclubhouse/quests/episode3/fizzicspuzzle1.py
+++ eosclubhouse/quests/episode3/fizzicspuzzle1.py
@@ -6,6 +6,7 @@ from eosclubhouse.system import Sound
 class FizzicsPuzzle1(Quest):
 
     __available_after_completing_quests__ = ['ApplyFob2']
+    GAME_PRESET = 1003
 
     def __init__(self):
         super().__init__('FizzicsPuzzle1', 'faber')
@@ -17,11 +18,20 @@ class FizzicsPuzzle1(Quest):
 
     @Quest.with_app_launched(Fizzics.APP_NAME)
     def step_goal(self):
+        self._app.set_js_property('preset', ('i', self.GAME_PRESET))
         self.show_hints_message('GOAL')
-        return self.step_success
+        return self.step_play
 
-    def step_success(self):
-        self.wait_confirm('SUCCESS')
+    @Quest.with_app_launched(Fizzics.APP_NAME)
+    def step_play(self):
+        self.wait_for_app_js_props_changed(self._app, ['quest3Success'])
+        if self._app.get_js_property('quest3Success') or self.debug_skip():
+            self.wait_confirm('SUCCESS')
+            return self.step_end
+
+        return self.step_play
+
+    def step_end(self):
         self.complete = True
         self.available = False
         Sound.play('quests/quest-complete')
--- eosclubhouse/quests/episode3/fizzicspuzzle2.py
+++ eosclubhouse/quests/episode3/fizzicspuzzle2.py
@@ -5,6 +5,8 @@ from eosclubhouse.system import Sound
 
 class FizzicsPuzzle2(Quest):
 
+    GAME_PRESET = 1004
+
     def __init__(self):
         super().__init__('FizzicsPuzzle2', 'faber')
         self._app = Fizzics()
@@ -15,12 +17,21 @@ class FizzicsPuzzle2(Quest):
 
     @Quest.with_app_launched(Fizzics.APP_NAME)
     def step_goal(self):
+        self._app.set_js_property('preset', ('i', self.GAME_PRESET))
         self.show_hints_message('GOAL')
-        return self.step_success
+        return self.step_play
 
-    def step_success(self):
-        self.wait_confirm('SUCCESS')
-        self.give_item('item.fob.3', consume_after_use=True)
+    @Quest.with_app_launched(Fizzics.APP_NAME)
+    def step_play(self):
+        self.wait_for_app_js_props_changed(self._app, ['quest4Success'])
+        if self._app.get_js_property('quest4Success') or self.debug_skip():
+            self.wait_confirm('SUCCESS')
+            self.give_item('item.fob.3', consume_after_use=True)
+            return self.step_end
+
+        return self.step_play
+
+    def step_end(self):
         self.wait_confirm('END')
         self.complete = True
         self.available = False
