dd3a3ee61ad881531005bcc231e936e046ef9936
bkarnow <ben@endlessm.com>
2019-04-22 12:46:20 -0700

    quests: update fizzicskey to use an actual choice! Dialogue trees, here we come.

--- eosclubhouse/quests/episode4/fizzicskey.py
+++ eosclubhouse/quests/episode4/fizzicskey.py
@@ -10,6 +10,32 @@ class FizzicsKey(Quest):
         super().__init__('FizzicsKey', 'Saniel')
         self._app = Fizzics()
 
+    def reset_params(self):
+        #TODO: What are the defualt values for these?
+        self._app.set_object_property('view.JSContext.globalParameters', 'gravity_0', ('u', 0))
+        self._app.set_object_property('view.JSContext.globalParameters', 'friction_0', ('u', 100))
+        self._app.set_object_property('view.JSContext.globalParameters', 'collision_0', ('u', 0))
+
+    def do_result_grav(self, result):
+        if result:
+            self._app.set_object_property('view.JSContext.globalParameters', 'gravity_0', ('u', 999))
+        else:
+            self._app.set_object_property('view.JSContext.globalParameters', 'gravity_0', ('u', 20))
+
+    def do_result_fric(self, result):
+        if result:
+            self._app.set_object_property('view.JSContext.globalParameters', 'friction_0', ('u', 999))
+        else:
+            self._app.set_object_property('view.JSContext.globalParameters', 'friction_0', ('u', 0))
+
+
+    def do_result_bounce(self, result):
+        if result:
+            self._app.set_object_property('view.JSContext.globalParameters', 'collision_0', ('u', 999))
+        else:
+            self._app.set_object_property('view.JSContext.globalParameters', 'collision_0', ('u', 0))
+
+
     def step_begin(self):
         self.ask_for_app_launch(self._app, pause_after_launch=2, message_id='DUMMY1')
         # get the player to the 'old' levels
@@ -21,16 +47,40 @@ class FizzicsKey(Quest):
 
     @Quest.with_app_launched(Fizzics.APP_NAME)
     def step_ingame(self):
-        self._app.set_object_property('view.JSContext.globalParameters', 'gravity_0', ('u', 0))
         self.wait_confirm('DUMMY1')
         self.wait_confirm('DUMMY2')
-        #ask the player what they want
-        #self.show_choices_message('SETFRIC', (('CHOICEHIGH', True), ('CHOICEHIGH', True))).wait()
-        #unfreeze everything
-        self._app.set_object_property('view.JSContext.globalParameters', 'gravity_0', ('u', 999))
-        #if goal great
-        #if not, reset time
+        #return self.step_success
+        return self.step_level1
+
+    @Quest.with_app_launched(Fizzics.APP_NAME)
+    def step_level1(self):
+        #say puzzle
+        #ask what grav
+        #set grav
+        #next level
+        self._app.set_object_property('view.JSContext.globalParameters', 'gravity_0', ('u', 0))
+        #for formatting and line length
+        choice1 = ('CHOICEHIGH', self.do_result_grav, True)
+        choice2 = ('CHOICELOW', self.do_result_grav, False)
+        self.show_choices_message('SETGRAV', choice1, choice2).wait()
+        self.wait_confirm('DUMMY3')
+        self.reset_params()
+        return self.step_level2
+        
+
+
+
+    @Quest.with_app_launched(Fizzics.APP_NAME)
+    def step_level2(self):
+        return self.step_level3
+
+
+
+    @Quest.with_app_launched(Fizzics.APP_NAME)
+    def step_level3(self):
         return self.step_success
+
+
 
     def step_success(self):
         self.wait_confirm('DUMMY3')
