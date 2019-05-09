49ac07a3f328c531c38b40164b14dffe35799597
bkarnow <ben@endlessm.com>
2019-05-09 09:21:57 -0700

    quests: Fixes for Leviathan Hackdex
    - Improving narrative navigation
    - Fixing spurious abort error
    - Helping players along more
    
    https://phabricator.endlessm.com/T26620

--- eosclubhouse/quests/episode4/levihackdex.py
+++ eosclubhouse/quests/episode4/levihackdex.py
@@ -6,6 +6,7 @@ class LeviHackdex(Quest):
 
     APP_NAME = 'com.endlessm.Hackdex_chapter_two'
     ROTATION = 'app.com_endlessm_Hackdex_chapter_two.encryption'
+    FLIP_COUNTER = 0
 
     def __init__(self):
         super().__init__('LeviHackdex', 'ada')
@@ -15,66 +16,74 @@ class LeviHackdex(Quest):
         self.ask_for_app_launch(self._app, pause_after_launch=2)
         return self.step_detect_progress
 
+    def is_unlocked(self):
+        lock_state = self.gss.get('lock.com.endlessm.Hackdex_chapter_two.1')
+        return lock_state is not None and not lock_state.get('locked')
+
     def step_detect_progress(self):
+        # new quest flow to better guide users -
+        # ask the user to flip, then give the key
+        # detect 2 flips and give further hints if the user hasn't gotten it
         # is the app running? launch it
         if not self._app.is_running():
             return self.step_begin
-        # app is running, do they already have the key?
-        key = self.gss.get('item.key.levi_hackdex.1')
-        if key is not None:
-            return self.step_wait_for_unlock
+        # app is running, have they already unlocked it?
+        if self.is_unlocked():
+            return self.step_wait_until_solved
         else:
             # no key, first play
-            return self.step_part1
+            return self.step_introduction
 
     @Quest.with_app_launched(APP_NAME)
-    def step_part1(self):
-        self.wait_confirm('OPENINTRO')
-        self.wait_confirm('OPENINTRO1')
-        self.wait_confirm('OPENINTRO2')
-        self.wait_confirm('OPENINTRO3')
-        self.wait_confirm('GIVEKEY')
+    def step_introduction(self):
+        for message_id in ['OPENINTRO', 'OPENINTRO1', 'OPENINTRO2', 'OPENINTRO3', 'OPENINTRO4',
+                           'PREFLIP', 'GIVEKEY']:
+            self.wait_confirm(message_id)
         self.give_item('item.key.levi_hackdex.1')
-        return self.step_detect_progress
+        return self.step_wait_for_unlock
 
     @Quest.with_app_launched(APP_NAME)
     def step_wait_for_unlock(self):
-        self.wait_confirm('FLIP')
-        lock_state = self.gss.get('lock.com.endlessm.Hackdex_chapter_two.1')
-        if lock_state is not None and not lock_state.get('locked'):
-            self.wait_confirm('DECRYPT')
+        if self.is_unlocked():
+            self.pause(1)
+            self.wait_confirm('UNLOCK')
+            self.pause(1)
             return self.step_wait_until_solved
         else:
-            self.wait_confirm('UNLOCK')
             return self.step_wait_for_unlock
 
     def step_wait_until_solved(self):
         if not self._app.is_running():
             return self.abort
-        # is the rotation correct?
+        # how many times did we flip, this session?
+        if self.FLIP_COUNTER == 0:
+            self.pause(3)
+            self.show_hints_message("DECRYPT")
+        if self.FLIP_COUNTER == 1:
+            self.pause(3)
+            self.show_hints_message("DECRYPT_ONE")
+        if self.FLIP_COUNTER == 2:
+            self.pause(3)
+            self.wait_confirm('DECRYPT_TWO')
+        # now check rotation
         data = self.gss.get('app.com_endlessm_Hackdex_chapter_two.encryption')
-        if data is None:
-            return self.abort
-
-        if data.get('rotation') == 5:
+        if data is not None and data.get('rotation') == 5:
             return self.step_part2
         # (From Hackdex 1)
         # The HackDex app is restarted when its parameters are changed and the app is flipped
         # back, so we check that and give it time before considering it has stopped running.
         self.connect_app_quit(self._app).wait()
         self.wait_for_app_launch(self._app, timeout=2)
+        self.FLIP_COUNTER += 1
         return self.step_wait_until_solved
 
     @Quest.with_app_launched(APP_NAME)
     def step_part2(self):
-        self.wait_confirm('DECRYPT_SUCCESS')
-        self.wait_confirm('BACKGROUND')
-        self.wait_confirm('PUSHINSTRUCTION')
-        self.wait_confirm('PUSHINSTRUCTION2')
-        self.wait_confirm('PUSHINSTRUCTION3')
-        self.wait_confirm('PUSHINSTRUCTION4')
-        self.wait_confirm('PUSHINSTRUCTION5')
-        self.wait_confirm('PUSHINSTRUCTION6')
+        self.pause(3)
+        for message_id in ['DECRYPT_SUCCESS', 'BACKGROUND', 'PUSHINSTRUCTION',
+                           'PUSHINSTRUCTION2', 'PUSHINSTRUCTION3', 'PUSHINSTRUCTION4',
+                           'PUSHINSTRUCTION5', 'PUSHINSTRUCTION6']:
+            self.wait_confirm(message_id)
         return self.step_success
 
     def step_success(self):
