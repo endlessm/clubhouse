97c73ccbd1be8676a8abb10aec487fe2161b9456
bkarnow <ben@endlessm.com>
2019-05-16 16:41:55 -0700

    quests: Show Sidetrack panel unlock message after unlocking
    https://phabricator.endlessm.com/T26686

--- eosclubhouse/quests/episode4/mazept2.py
+++ eosclubhouse/quests/episode4/mazept2.py
@@ -10,6 +10,16 @@ class MazePt2(Quest):
     def __init__(self):
         super().__init__('MazePt2', 'ada')
         self._app = Sidetrack()
+        self.unlocked = False
+
+    @Quest.with_app_launched(Sidetrack.APP_NAME)
+    def step_wait_for_unlock(self):
+        if self.is_panel_unlocked('lock.sidetrack.1'):
+            self.unlocked = True
+            return self.step_play_level
+        else:
+            self.pause(1)
+            return self.step_wait_for_unlock
 
     def step_begin(self):
         self.ask_for_app_launch(self._app, pause_after_launch=2, message_id='LAUNCH')
@@ -18,15 +28,20 @@ class MazePt2(Quest):
         if 23 > highest_level > 28:
             self._app.set_js_property('highestAchievedLevel', ('u', 23))
         self._app.set_js_property('showHackdex', ('b', highest_level <= 26))
+        if self.is_panel_unlocked('lock.sidetrack.1'):
+            self.unlocked = True
         return self.step_play_level
 
     @Quest.with_app_launched(Sidetrack.APP_NAME)
     def step_play_level(self):
         current_level = int(self._app.get_js_property('currentLevel'))
         if current_level == 23:
-            self.show_hints_message('FLIP')
-            self.wait_for_app_js_props_changed(self._app, ['flipped'])
-            self.show_hints_message('INSTRUCTIONS')
+            if not self.unlocked:
+                self.show_hints_message('FLIP')
+                return self.step_wait_for_unlock
+            else:
+                self.pause(4)
+                self.show_hints_message('INSTRUCTIONS')
         elif current_level == 24:
             self.wait_confirm('LEVELS2')
         elif current_level == 25:
