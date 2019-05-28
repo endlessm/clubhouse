575fbb8d476223ad390cec1beab4b3fec196446f
Joaquim Rocha <jrocha@endlessm.com>
2019-05-28 19:00:06 +0200

    tests: Test the recent Quest.get_quest_dependencies method
    https://phabricator.endlessm.com/T26812

--- tests/test_quests.py
+++ tests/test_quests.py
@@ -1,6 +1,6 @@
 from eosclubhouse.libquest import Registry, NoMessageIdError
 from eosclubhouse.utils import QuestStringCatalog
-from clubhouseunittest import ClubhouseTestCase, define_quest
+from clubhouseunittest import ClubhouseTestCase, define_quest, define_quest_set, setup_episode
 
 
 class TestQuests(ClubhouseTestCase):
@@ -24,3 +24,32 @@ class TestQuests(ClubhouseTestCase):
 
         with self.assertRaises(NoMessageIdError):
             quest.show_message('INEXISTENT_MESSAGE')
+
+    def test_verify_dependencies(self):
+        """Tests that Quests compute their dependencies correctly, taking into account the
+        quests coming before them in the QuestSet but also their own dependencies."""
+
+        def get_dependencies(quest_id):
+            quest = Registry.get_quest_by_name(quest_id)
+            return [quest_dep.get_id() for quest_dep in quest.get_dependency_quests()]
+
+        PhonyAlice = define_quest_set('PhonyAlice', 'Alice',
+                                      [('QuestA0', []),
+                                       ('QuestB0', []),
+                                       ('QuestC0', []),
+                                       ('QuestD0', ['QuestB1'])])
+
+        PhonyBob = define_quest_set('PhonyBob', 'Bob',
+                                    [('QuestA1', []),
+                                     ('QuestB1', ['QuestC0'])])
+
+        setup_episode([PhonyAlice(), PhonyBob()])
+
+        self.assertEqual(get_dependencies('QuestD0'),
+                         ['QuestC0', 'QuestB0', 'QuestA0', 'QuestB1', 'QuestA1'])
+
+        self.assertEqual(get_dependencies('QuestB1'), ['QuestA1', 'QuestC0', 'QuestB0', 'QuestA0'])
+
+        self.assertEqual(get_dependencies('QuestC0'), ['QuestB0', 'QuestA0'])
+
+        self.assertEqual(get_dependencies('QuestA1'), [])
