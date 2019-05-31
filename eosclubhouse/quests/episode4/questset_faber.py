5b74c73254e5ae68cb314c31060ab111d8156420
Joaquim Rocha <jrocha@endlessm.com>
2019-05-31 22:27:03 +0200

    quests: Remove useless construction overide in episode4
    The Faber and Saniel quest sets were overriding the constructor but
    not doing anything with it, so it's better to just not define it and
    have a simpler code.
    
    https://phabricator.endlessm.com/T26875

--- eosclubhouse/quests/episode4/questset_faber.py
+++ eosclubhouse/quests/episode4/questset_faber.py
@@ -6,8 +6,5 @@ class FaberQuestSet(QuestSet):
     __character_id__ = 'faber'
     __quests__ = ['LightspeedKey']
 
-    def __init__(self):
-        super().__init__()
-
 
 Registry.register_quest_set(FaberQuestSet)
--- eosclubhouse/quests/episode4/questset_saniel.py
+++ eosclubhouse/quests/episode4/questset_saniel.py
@@ -6,8 +6,5 @@ class SanielQuestSet(QuestSet):
     __character_id__ = 'saniel'
     __quests__ = ['FizzicsKey']
 
-    def __init__(self):
-        super().__init__()
-
 
 Registry.register_quest_set(SanielQuestSet)
