0b3598f48b39078dde42bebe8d3b6786adb6fe4a
bkarnow <ben@endlessm.com>
2019-06-04 10:58:01 -0700

    Depth on demand experiment is working (talk to Ada)

--- eosclubhouse/quests/episodenext/adaquest1.py
+++ eosclubhouse/quests/episodenext/adaquest1.py
@@ -9,34 +9,34 @@ class AdaQuest1(Quest):
     def __init__(self):
         super().__init__('AdaQuest1', 'ada')
         self._app = App(self.APP_NAME)
-        # self.deep_messages = []
+        self.depth_flag = False
 
     def step_begin(self):
         self.ask_for_app_launch(self._app, pause_after_launch=2)
         return self.step_main
 
-    def cancel_deepness(self):
-        self.wait_confirm('REVENON')
-        # return self.step_success
+    def set_depth_flag(self, value):
+        self.depth_flag = True if value else False
 
-    def proceed_deeper(self, deep_messages):
-        for message in deep_messages:
-            self.wait_confirm(message)
-
-    def show_deepness(self, deep_messages):
-        proceed = ('PROCEED', self.proceed_deeper(deep_messages), 1)
-        cancel = ('CANCEL', self.cancel_deepness, 1)
-        self.show_choices_message('INITIALTEXT', proceed, cancel).wait()
+    @Quest.with_app_launched(APP_NAME)
+    def show_deepness(self, offer, deep_messages):
+        proceed = ('PROCEED', self.set_depth_flag, True)
+        cancel = ('CANCEL', self.set_depth_flag, False)
+        self.show_choices_message(offer, proceed, cancel).wait()
+        if self.depth_flag:
+            for message in deep_messages:
+                self.wait_confirm(message)
+        else:
+            self.wait_confirm('REVENON')
 
     @Quest.with_app_launched(APP_NAME)
     def step_main(self):
-        # self.wait_confirm('INITIALTEXT')
-        self.show_deepness(['DEEPTEXT1', 'DEEPTEXT2', 'DEEPTEXT3'])
+        self.show_deepness('INITIALTEXT', ['DEEPTEXT1', 'DEEPTEXT2', 'DEEPTEXT3'])
         return self.step_success
 
     def step_success(self):
         self.wait_confirm('SUCCESS')
-        self.complete = True
-        self.available = False
+        # self.complete = True
+        # self.available = False
         Sound.play('quests/quest-complete')
         self.stop()
