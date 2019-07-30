44744e9a0ef2a4253f67771784d415a4f46bbdc0
Manuel Quiñones <manuel.por.aca@gmail.com>
2019-07-30 14:38:53 -0300

    quests: Add demo for narrative quests
    To show how the API can be used
    
    https://phabricator.endlessm.com/T27307

--- eosclubhouse/quests/hack2/testa1.py
+++ eosclubhouse/quests/hack2/testa1.py
@@ -10,10 +10,31 @@ class TestA1(Quest):
     __pathway_order__ = 100
 
     def step_begin(self):
-        self.wait_confirm('BEGIN')
-        return self.step_end
+        self.show_message('NARRATIVE_A', narrative=True)
+        self.pause(3)
+        self.show_message('NARRATIVE_B', narrative=True)
+        self.pause(3)
+        self.dismiss_message(narrative=True)
+        self.pause(1)
+        # @todo: Check why the button isn't displayed the first time,
+        # allocation issues.
+        # self.wait_confirm('NARRATIVE_C', narrative=True, confirm_label='Next')
+        return self.step_again(first_try=True)
+
+    def step_again(self, first_try=False, said_yes=False):
+        if first_try:
+            message_id = 'NARRATIVE_START'
+        else:
+            message_id = 'NARRATIVE_YES' if said_yes else 'NARRATIVE_NO'
+        self.show_message(message_id, narrative=True,
+                          choices=[
+                              ('Yes', self.step_again, False, True),
+                              ('No', self.step_again, False, False),
+                              ('Bye', self.step_end),
+                          ])
 
     def step_end(self):
+        self.dismiss_message(narrative=True)
         self.complete = True
         self.available = False
         self.show_message('END', choices=[('Bye', self.stop)])
