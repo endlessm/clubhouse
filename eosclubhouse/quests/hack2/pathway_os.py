d2fd7250316d6320a44424ee6462aef5abaa0872
bkarnow <ben@endlessm.com>
2019-07-29 17:51:47 -0700

    quests: Add files and content for non-narrative quests
    All currently known T2 games.
    
    - Also added data for other pathways, why not
    
    - Characters are limited to Saniel and Ada, the others need data
      before they can have quests associated
    
    https://phabricator.endlessm.com/T27238
    https://phabricator.endlessm.com/T27425

---
+++ eosclubhouse/quests/hack2/pathway_os.py
@@ -0,0 +1,9 @@
+from eosclubhouse.libquest import PathWay, Registry
+
+
+class OSPathWay(PathWay):
+
+    __pathway_name__ = 'Operating System'
+
+
+Registry.register_quest_set(OSPathWay)
---
+++ eosclubhouse/quests/hack2/t2teddy.py
@@ -0,0 +1,31 @@
+from eosclubhouse.libquest import Quest
+from eosclubhouse.system import App, Sound
+
+
+class T2Teddy(Quest):
+
+    APP_NAME = 'com.endlessnetwork.MidnightmareTeddy'
+
+    __quest_name__ = 'Terminal 2 - Midnightmare Teddy'
+    __tags__ = ['mission:saniel', 'pathway:games', 'difficulty:normal']
+    __mission_order__ = 130
+    __pathway_order__ = 130
+
+    def setup(self):
+        self._app = App(self.APP_NAME)
+
+    def step_begin(self):
+        self.wait_confirm('GREET1')
+        self.show_message('GREET2', choices=[('Cool!', self.step_launch)])
+
+    def step_launch(self):
+        Sound.play('quests/quest-complete')
+        # We are about to launch a fullscreen app. So no messages
+        # should be displayed after this point:
+        self._app.launch()
+        self.wait_for_app_launch(self._app, pause_after_launch=3)
+        return self.step_end
+
+    def step_end(self):
+        self.complete = True
+        self.available = False
