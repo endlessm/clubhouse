0b1539a934bbf784af06c9a52597a29b480f62b6
bkarnow <ben@endlessm.com>
2019-08-07 19:03:49 -0700

    Trying to get the choice to display properly

--- eosclubhouse/quests/hack2/story_ada1.py
+++ eosclubhouse/quests/hack2/story_ada1.py
@@ -21,26 +21,26 @@ class Story_Ada1(Quest):
         # is it the last step?
         #   goto end
         #
-        for step in range(1, self.__total_steps__):
+        for step in range(1, self.__total_steps__ + 1):
             message_id = 'STORY_ADA1_' + str(step)
             if step in self.__question_steps__:
                 self.ask_question(message_id)
             else:
-                self.wait_confirm(message_id, narrative=True, confirm_label='>')
+                self.wait_confirm(message_id, narrative=True)
         return self.step_end
 
     def ask_question(self, proposal_string):
         # u"\U0001F44D"     /    u"\U0001F44E"
         # U+1F44D for thumbs up, U+1F44E for down
-        self.show_message(proposal_string, narrative=True, choices=[
-            (u"\U0001F44D", self.ask_question, proposal_string, True),
-            (u"\U0001F44E", self.ask_question, proposal_string, False)])
+        self.show_choices_message(proposal_string, narrative=True, choices=[
+            (u"\U0001F44D", self.display_answer, proposal_string, True),
+            (u"\U0001F44E", self.display_answer, proposal_string, False)])
 
     def display_answer(self, proposal_string, answer):
         if answer:
-            self.wait_confirm(proposal_string + '_RPOS', narrative=True, confirm_label='>')
+            self.wait_confirm(proposal_string + '_RPOS', narrative=True)
         else:
-            self.wait_confirm(proposal_string + '_RNEG', narrative=True, confirm_label='>')
+            self.wait_confirm(proposal_string + '_RNEG', narrative=True)
 
     def step_end(self):
         self.dismiss_message(narrative=True)
