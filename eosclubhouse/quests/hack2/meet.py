5291e0d495d26d7a29325fcf33158b190a8c6edf
bkarnow <ben@endlessm.com>
2019-08-28 18:18:21 -0700

    T27628 - Allow for a savestate in meet.py to deal with the user disabling and re-enabling hack mode - currently not working because the quest doesn't re-offer

--- eosclubhouse/quests/hack2/meet.py
+++ eosclubhouse/quests/hack2/meet.py
@@ -14,18 +14,34 @@ class Meet(Quest):
         self.skippable = True
 
     def step_begin(self):
-        self.wait_confirm('WELCOME1')
-        self.wait_confirm('WELCOME2')
-        # check to see if the player got a hint in the initial puzzle
-        # (this is intended to reward existing users who know the puzzle from hack 1.0)
-        if self.get_named_quest_conf('FirstContact', 'puzzle_hint_given'):
-            msg_id = 'WELCOME3_GOTHINT'
+
+        resume_at_step = self.get_named_quest_conf('Meet', 'resume_step')
+
+        if resume_at_step == "main":
+            return self.step_mainwindow
+        elif resume_at_step == "hack":
+            return self.step_hackmode
+        elif resume_at_step == "path":
+            return self.step_pathways
+        elif resume_at_step == "profile":
+            return self.step_profile
+        elif resume_at_step == "end":
+            return self.step_end
         else:
-            msg_id = 'WELCOME3_NOHINT'
-        self.wait_confirm(msg_id)
-        return self.step_mainwindow
+            self.wait_confirm('WELCOME1')
+            self.wait_confirm('WELCOME2')
+            # check to see if the player got a hint in the initial puzzle
+            # (this is intended to reward existing users who know the puzzle from hack 1.0)
+            if self.get_named_quest_conf('FirstContact', 'puzzle_hint_given'):
+                msg_id = 'WELCOME3_GOTHINT'
+            else:
+                msg_id = 'WELCOME3_NOHINT'
+            self.wait_confirm(msg_id)
+            return self.step_mainwindow
 
     def step_mainwindow(self):
+        self.set_conf('resume_step', 'main')
+        self.save_conf()
         # explain the concept of the clubhouse
         self.wait_confirm('EXPLAIN_MAIN1')
         self.wait_confirm('EXPLAIN_MAIN2')
@@ -33,18 +49,24 @@ class Meet(Quest):
 
     def step_hackmode(self):
         # explain hack mode and how it works
+        self.set_conf('resume_step', 'hack')
+        self.save_conf()
         for msgid in ['EXPLAIN_HACK1', 'EXPLAIN_HACK2', 'EXPLAIN_HACK3', 'EXPLAIN_HACK4']:
             self.wait_confirm(msgid)
         return self.step_pathways
 
     def step_pathways(self):
         # explain pathways
+        self.set_conf('resume_step', 'path')
+        self.save_conf()
         self.wait_confirm('EXPLAIN_PATHWAYS1')
         self.wait_confirm('EXPLAIN_PATHWAYS2')
         return self.step_profile
 
     def step_profile(self):
         # explain the profile
+        self.set_conf('resume_step', 'profile')
+        self.save_conf()
         for msgid in ['EXPLAIN_PROFILE1', 'EXPLAIN_PROFILE2', 'EXPLAIN_PROFILE3']:
             self.wait_confirm(msgid)
         # ask if player wants to change their name
@@ -56,10 +78,12 @@ class Meet(Quest):
 
     def step_changename(self, result):
         if result:
-            for msgid in ['CHANGE_NAME1', 'CHANGE_NAME2', 'CHANGE_NAME3']:
+            for msgid in ['CHANGE_NAME1', 'CHANGE_NAME2', 'CHANGE_NAME3', 'CHANGE_NAME4']:
                 self.wait_confirm(msgid)
 
     def step_end(self):
+        self.set_conf('resume_step', 'end')
+        self.save_conf()
         self.wait_confirm('END1')
         self.wait_confirm('END2')
         self.complete = True
