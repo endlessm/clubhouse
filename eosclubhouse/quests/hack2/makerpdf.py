5213872c17615ed36835c18827bd6f221889e73d
bkarnow <ben@endlessm.com>
2019-09-20 18:36:11 -0700

    maker pdf quest

---
+++ eosclubhouse/quests/hack2/makerpdf.py
@@ -0,0 +1,15 @@
+from eosclubhouse.libquest import Quest
+import os
+
+
+class MakerPDF(Quest):
+
+    __quest_name__ = 'Home-grown Earthquake'
+    __tags__ = ['mission:faber']
+    __mission_order__ = 200
+
+    def step_begin(self):
+        self.deploy_file('earthquake.pdf', '~/', override=True)
+        self.wait_confirm('WELCOME')
+        os.system('gio open earthquake.pdf')
+        return self.step_complete_and_stop
