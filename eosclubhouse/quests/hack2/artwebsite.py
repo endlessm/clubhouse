8f780aeecc8a1c7193c1b94ee9b44cec2ac045eb
bkarnow <ben@endlessm.com>
2019-09-20 18:40:25 -0700

    Art website quest (processing)

---
+++ eosclubhouse/quests/hack2/artwebsite.py
@@ -0,0 +1,14 @@
+from eosclubhouse.libquest import Quest
+import os
+
+
+class ArtWebsite(Quest):
+
+    __quest_name__ = 'A Taste of Processing'
+    __tags__ = ['mission:estelle']
+    __mission_order__ = 400
+
+    def step_begin(self):
+        self.wait_confirm('WELCOME')
+        os.system('xdg-open https://editor.p5js.org/p5/sketches/Hello_P5:_shapes')
+        return self.step_complete_and_stop
