b005d1e736aed8e3d920eaef452ee4219d720768
bkarnow <ben@endlessm.com>
2019-09-24 16:02:24 -0700

    renaming and re-uploading earthquake PDF
    ... we'll leave the directory detection for later

--- eosclubhouse/quests/hack2/pdfearthquake.py
+++ eosclubhouse/quests/hack2/pdfearthquake.py
@@ -9,7 +9,7 @@ class PDFEarthquake(Quest):
     __mission_order__ = 200
 
     def step_begin(self):
-        self.deploy_file('earthquake.pdf', '~/', override=True)
+        self.deploy_file('earthquake_at_home.pdf', '~/Documents/', override=True)
         self.wait_confirm('WELCOME')
-        os.system("flatpak-spawn --host evince ~/earthquake.pdf")
+        os.system("flatpak-spawn --host evince ~/Documents/earthquake_at_home.pdf")
         return self.step_complete_and_stop
