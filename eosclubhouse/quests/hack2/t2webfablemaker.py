22c3a85a705545165c189dfd7cebde5dfd99d082
bkarnow <ben@endlessm.com>
2019-10-22 14:58:20 -0700

    quests: Add FableMaker to Games Pathway
    https://phabricator.endlessm.com/T28292

---
+++ eosclubhouse/quests/hack2/t2webfablemaker.py
@@ -0,0 +1,25 @@
+from eosclubhouse.libquest import Quest
+
+
+class T2WebFableMaker(Quest):
+
+    __app_id__ = 'com.endlessnetwork.fablemaker'
+    __tags__ = ['pathway:games', 'difficulty:easy']
+    __pathway_order__ = 150
+
+    def step_begin(self):
+        if not self.app.is_installed():
+            self.wait_confirm('NOTINSTALLED', confirm_label='App Center, got it.')
+            return self.step_abort
+        return self.step_instruct
+
+    def step_instruct(self):
+        self.wait_confirm('GREET1')
+        self.wait_confirm('GREET2', confirm_label='Interesting!')
+        return self.step_launch
+
+    def step_launch(self):
+        self.app.launch()
+        self.wait_for_app_in_foreground()
+        self.wait_for_app_in_foreground(in_foreground=False)
+        return self.step_complete_and_stop
