b886a9507a6f5134569add37ad19a49af48184b0
Daniel García Moreno <daniel@endlessm.com>
2019-10-30 11:25:01 +0100

    splash: Add new splash assets with animation
    https://phabricator.endlessm.com/T28271

--- eosclubhouse/splash.py
+++ eosclubhouse/splash.py
@@ -1,6 +1,10 @@
+import os
+
 from gi.repository import Gdk
 from gi.repository import Gtk
 from gi.repository import GLib
+
+from eosclubhouse.animation import AnimationImage
 
 
 # Show spinning icon while loading splash.
@@ -15,27 +19,35 @@ class SplashWindow(Gtk.ApplicationWindow):
                          decorated=False,
                          skip_pager_hint=True,
                          skip_taskbar_hint=True,
-                         default_width=480,
-                         default_height=360)
+                         default_width=400,
+                         default_height=300)
 
         self.set_visual(self.get_screen().get_rgba_visual())
         self.set_keep_above(True)
 
         self._css_provider = Gtk.CssProvider()
-        self._css_provider.load_from_data("\
-        window.splash {\
-            background-color: unset;\
-            background: url('/app/share/eos-clubhouse/splash.svg');\
-            background-size: cover;\
-            transition: opacity 1s ease-in-out;\
-        }\
-        window.splash.fadeout {\
-            opacity: 0;\
-        }".encode())
+        self._css_provider.load_from_data("""
+        window.splash {
+            background-color: unset;
+            background-size: cover;
+            transition: opacity 1s ease-in-out;
+        }
+        window.splash.fadeout {
+            opacity: 0;
+        }
+        """.encode())
         ctx = self.get_style_context()
         ctx.add_class('splash')
         ctx.add_provider_for_screen(self.get_screen(), self._css_provider,
                                     Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+        path = os.path.join('splash', 'loading')
+        self._loading = AnimationImage(path)
+        self._loading.load()
+        self._loading.show()
+        self.add(self._loading)
+
+        self._loading.play('default')
 
     def destroy_with_fadeout(self):
         self.get_style_context().add_class('fadeout')
