b8625abf7e020b54fe9bb80e2b3b27d2fa4a697c
Manuel Quiñones <manuel.por.aca@gmail.com>
2019-10-30 11:39:52 -0300

    quests: Improve migration quest
    Authored by Ben, merged with changes.
    
    https://phabricator.endlessm.com/T27986

--- eosclubhouse/quests/hack2/migration.py
+++ eosclubhouse/quests/hack2/migration.py
@@ -1,15 +1,58 @@
 from eosclubhouse.libquest import Quest
-from eosclubhouse.system import Desktop
+from eosclubhouse.system import App, Desktop
 
 
 class Migration(Quest):
 
-    __tags__ = ['pathway:games']
+    __tags__ = ['pathway:games', 'skillset:Veteran']
 
     def setup(self):
         self.skippable = True
 
     def step_begin(self):
+        for msgid in ['WELCOME', 'WELCOME2']:
+            self.wait_confirm(msgid)
+        return self.step_open_clubhouse
+
+    def step_open_clubhouse(self):
+        self.show_hints_message('OPENIT')
         Desktop.set_hack_icon_pulse(True)
-        self.wait_confirm('WELCOME')
+        self.wait_for_app_in_foreground(App('com.hack_computer.Clubhouse'))
+        return self.step_explain_old_apps
+
+    def step_explain_old_apps(self):
+        # explain what we did to the old apps
+        for msgid in ['OLDSTUFF1', 'OLDSTUFF2', 'OLDSTUFF3',
+                      'OLDSTUFF4', 'OLDSTUFF5']:
+            self.wait_confirm(msgid)
+        return self.step_explain_new_stuff
+
+    def step_explain_new_stuff(self):
+        # explain hack mode
+        for msgid in ['NEWSTUFF', 'HACKMODE1', 'HACKMODE2',
+                      'HACKMODE3', 'HACKMODE4', 'HACKMODE5']:
+            self.wait_confirm(msgid)
+        return self.step_explain_activities
+
+    def step_explain_activities(self):
+        # explain activities and how to play them
+        for msgid in ['ACTIVITIES1', 'ACTIVITIES2', 'ACTIVITIES3']:
+            self.wait_confirm(msgid)
+        return self.step_explain_profile
+
+    def step_explain_profile(self):
+        # explain how the profile works and how to change your name
+        for msgid in ['PROFILE1', 'PROFILE2', 'PROFILE3']:
+            self.wait_confirm(msgid)
+        action = self.show_choices_message('PROFILE_ASK', ('PROFILE_POS', None, True),
+                                           ('PROFILE_NEG', None, False)).wait()
+        if action.future.result():
+            for msgid in ['PROFILE_CHANGENAME1', 'PROFILE_CHANGENAME2',
+                          'PROFILE_CHANGENAME3', 'PROFILE_CHANGENAME4']:
+                self.wait_confirm(msgid)
+        return self.step_last
+
+    def step_last(self):
+        self.wait_confirm('END1')
+        self.wait_confirm('END2')
         return self.step_complete_and_stop
