4bbdaa2705c26bf1e07c322368ff0c34aa03421d
bkarnow <ben@endlessm.com>
2019-11-04 10:43:01 -0800

    quests: HTML quest improvements
    - Abigail's edits integrated
    - Fix for codepens opening even if initial dialogues are dismissed
    - Added outro text
    
    https://phabricator.endlessm.com/T28518

--- eosclubhouse/quests/hack2/htmlintro3.py
+++ eosclubhouse/quests/hack2/htmlintro3.py
@@ -7,7 +7,7 @@ class HTMLIntro3(Quest):
     __tags__ = ['pathway:web', 'difficulty:easy', 'skillset:LaunchQuests']
     __pathway_order__ = 503
 
-    TOTAL_MESSAGES = 26
+    TOTAL_MESSAGES = 31
 
     def setup(self):
         return self.step_begin
@@ -15,12 +15,16 @@ class HTMLIntro3(Quest):
     def step_begin(self):
         self.wait_confirm('1')
         self.wait_confirm('2')
+        self.wait_confirm('3')
+        return self.step_launch
+
+    def step_launch(self):
         os.system('xdg-open https://codepen.io/madetohack/pen/oNNYNXB?editors=1000#code-area')
-        return self.step_main_loop, 3
+        return self.step_main_loop, 4
 
     def step_main_loop(self, message_index):
         if message_index > self.TOTAL_MESSAGES:
-            self.wait_confirm('END')
+            self.wait_confirm('END', confirm_label='A secret? Cool!')
             return self.step_complete_and_stop
         elif message_index < 1:
             message_index = 1
--- eosclubhouse/quests/hack2/htmlintro4.py
+++ eosclubhouse/quests/hack2/htmlintro4.py
@@ -7,20 +7,22 @@ class HTMLIntro4(Quest):
     __tags__ = ['pathway:web', 'difficulty:easy', 'skillset:LaunchQuests']
     __pathway_order__ = 504
 
-    TOTAL_MESSAGES = 20
+    TOTAL_MESSAGES = 24
 
     def setup(self):
         return self.step_begin
 
     def step_begin(self):
         self.wait_confirm('1')
-        self.wait_confirm('2')
-        os.system('xdg-open https://codepen.io/madetohack/pen/vYYyNba?editors=1000')
-        return self.step_main_loop, 3
+        return self.step_launch
+
+    def step_launch(self):
+        os.system('xdg-open https://codepen.io/madetohack/pen/vYYyNba?editors=1000#code-area')
+        return self.step_main_loop, 2
 
     def step_main_loop(self, message_index):
         if message_index > self.TOTAL_MESSAGES:
-            self.wait_confirm('END')
+            self.wait_confirm('END', confirm_label='See you soon!')
             return self.step_complete_and_stop
         elif message_index < 1:
             message_index = 1
