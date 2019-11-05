5bfe38551ebd0536a6942799998c32040d3f6e27
bkarnow <ben@endlessm.com>
2019-11-04 17:17:18 -0800

    quests: Quicker still quickstart
    - Swapping some things between Meet and Quickstart
    - Adding highlighting to Meet
    - Porting over Manuel's fake hack switch
    
    https://phabricator.endlessm.com/T28445

--- eosclubhouse/quests/hack2/meet.py
+++ eosclubhouse/quests/hack2/meet.py
@@ -16,22 +16,29 @@ class Meet(Quest):
         return self.step_mainwindow
 
     def step_mainwindow(self):
+        self.highlight_nav('CLUBHOUSE')
         self.wait_confirm('GOBACK')
         # explain the concept of the clubhouse
+        self.highlight_nav('')
         self.wait_confirm('EXPLAIN_MAIN1')
         self.wait_confirm('EXPLAIN_MAIN2')
         return self.step_hackmode
 
     def step_hackmode(self):
         # explain hack mode and how it works
-        for msgid in ['EXPLAIN_HACK1', 'EXPLAIN_HACK2', 'EXPLAIN_HACK3', 'EXPLAIN_HACK4']:
+        self.wait_confirm('EXPLAIN_HACK1')
+        self._clubhouse_state.hack_switch_highlighted = True
+        for msgid in ['EXPLAIN_HACK2', 'EXPLAIN_HACK3', 'EXPLAIN_HACK4']:
             self.wait_confirm(msgid)
+        self._clubhouse_state.hack_switch_highlighted = False
         return self.step_pathways
 
     def step_pathways(self):
         # explain pathways
+        self.highlight_nav('PATHWAYS')
         self.wait_confirm('EXPLAIN_PATHWAYS1')
         self.wait_confirm('EXPLAIN_PATHWAYS2')
+        self.highlight_nav('')
         return self.step_profile
 
     def step_profile(self):
@@ -51,9 +58,10 @@ class Meet(Quest):
         choice = action.future.result()
 
         if choice:
-            for msgid in ['CHANGE_NAME1', 'CHANGE_NAME2', 'CHANGE_NAME3']:
+            for msgid in ['CHANGE_NAME1', 'CHANGE_NAME2', 'CHANGE_NAME3', 'CHANGE_NAME4']:
                 self.wait_confirm(msgid)
 
-        for msgid in ['END1', 'END2', 'END3']:
+        for msgid in ['END1', 'END2']:
             self.wait_confirm(msgid)
+        self.wait_confirm('END3', confirm_label="See you!")
         return self.step_complete_and_stop
