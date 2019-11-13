0273177cafd38a136b71482bf2742a46007df044
bkarnow <ben@endlessm.com>
2019-11-13 13:54:20 -0800

    quests: Adding install detector for Project Library quests - Also adding tags! Yikes.
    https://phabricator.endlessm.com/T28591

--- eosclubhouse/quests/hack2/artsnapshot.py
+++ eosclubhouse/quests/hack2/artsnapshot.py
@@ -7,13 +7,20 @@ class ArtSnapshot(Quest):
     APP_NAME = 'com.hack_computer.ProjectLibrary'
     ARTICLE_NAME = 'Snapshot'
 
-    __tags__ = ['pathway:art']
+    __tags__ = ['pathway:art', 'difficulty:easy', 'skillset:LaunchQuests']
     __pathway_order__ = 400
 
     def setup(self):
         self._app = App(self.APP_NAME)
 
     def step_begin(self):
+        if not self._app.is_installed():
+            self.wait_confirm('NOQUEST_PROJLIB_NOTINSTALLED', confirm_label='App Center, got it.')
+            return self.step_abort
+        else:
+            return self.step_instruct
+
+    def step_instruct(self):
         self.wait_confirm('WELCOME', confirm_label='Interesting...')
         self._app.open_article(self.ARTICLE_NAME)
         return self.step_complete_and_stop
