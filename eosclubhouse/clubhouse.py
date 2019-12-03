b251b767961d96f1007ed32590c582da6894216f
Daniel García Moreno <daniel@endlessm.com>
2019-12-03 14:16:42 +0100

    clubhouse: User button only visible in home view
    https://phabricator.endlessm.com/T28845

--- eosclubhouse/clubhouse.py
+++ eosclubhouse/clubhouse.py
@@ -1866,15 +1866,15 @@ class ClubhouseWindow(Gtk.ApplicationWindow):
         if current_page == new_page:
             return
 
-        if page_name == 'CLUBHOUSE':
-            self._hack_news_button.set_active(False)
-            self._clubhouse_button.set_active(True)
-        elif page_name == 'NEWS':
-            self._clubhouse_button.set_active(False)
-            self._hack_news_button.set_active(True)
+        self._clubhouse_button.active = new_page == 'CLUBHOUSE'
+        self._hack_news_button.set_active(new_page == 'NEWS')
+
+        if new_page in ['CLUBHOUSE', 'NEWS']:
+            self._user_image_button.show()
+            self._user_box.show()
         else:
-            self._clubhouse_button.set_active(False)
-            self._hack_news_button.set_active(False)
+            self._user_image_button.hide()
+            self._user_box.hide()
 
         if current_page == 'CHARACTER':
             self.hide_achievements_view()
@@ -1960,10 +1960,14 @@ class ClubhouseWindow(Gtk.ApplicationWindow):
 
     @Gtk.Template.Callback()
     def _user_button_revealer_reveal_child_cb(self, _button, _pspec):
+        current_page = self._stack.get_visible_child_name()
+
         if self._user_button_revealer.props.child_revealed:
             self._headerbar_box.get_style_context().remove_class('profile')
+            self._clubhouse_button.active = current_page == 'CLUBHOUSE'
         else:
             self._headerbar_box.get_style_context().add_class('profile')
+            self._clubhouse_button.active = False
 
     @Gtk.Template.Callback()
     def _achievements_view_revealer_child_revealed_notify_cb(self, revealer, _pspec):
