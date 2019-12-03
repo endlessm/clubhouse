5ab7c2b4cfe4b4f6d3be87e81997122d6e18210d
Manuel Quiñones <manuel.por.aca@gmail.com>
2019-12-03 16:04:15 -0300

    tools: Add scripts to compile the CSS
    - tools/compile-css: Similar to get-strings-file, will be used to
      compile the CSS from the SASS file.
    
    - tools/check-css: Similar to check-strings, will be used to check if
      the CSS is out of sync with the SASS file.
    
    Also:
    
    - Add libsass to the Python virtual environment.
    
    - Check if CSS is out of sync in the pre-commit hook, and print
      warning if not.
    
    - Commit the CSS in tools/update-strings.sh, called by the continuous
      integration tool.
    
    https://phabricator.endlessm.com/T28792

--- tools/pre-commit-hook.sh
+++ tools/pre-commit-hook.sh
@@ -10,6 +10,9 @@
 source_dir="$(git rev-parse --show-toplevel)"
 
 $source_dir/tools/run-lint || exit
+if [ -z $SKIP_COMPILE_CSS ]; then
+    $source_dir/tools/check-css || true
+fi
 if [ -z $SKIP_STRINGS_CHECK ]; then
     $source_dir/tools/check-strings || true
 fi
