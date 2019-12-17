c96c0cbe2fa176d50b24ae627a5424ae25b71858
bkarnow <ben@endlessm.com>
2019-12-17 15:36:26 -0800

    quests: Add OS CYOA quest
    https://phabricator.endlessm.com/T29010

---
+++ eosclubhouse/quests/hack2/osadventure.py
@@ -0,0 +1,35 @@
+from eosclubhouse.libquest import Quest
+
+
+class OSAdventure(Quest):
+
+    __app_id__ = 'org.gnome.Terminal'
+    __tags__ = ['pathway:operating system', 'difficulty:medium']
+    __pathway_order__ = 410
+
+    def setup(self):
+        self._info_messages = self.get_loop_messages('OSADVENTURE')
+
+    def step_begin(self):
+        self.wait_confirm('GREET')
+        return self.step_launch
+
+    def step_launch(self):
+        self.ask_for_app_launch(message_id='LAUNCH')
+        return self.step_main_loop
+
+    def step_main_loop(self, message_index=0):
+        message_id = self._info_messages[message_index]
+
+        if message_id == self._info_messages[-1]:
+            self.wait_confirm(message_id, confirm_label="Cool!")
+            return self.step_complete_and_stop
+
+        options = []
+        if message_id != self._info_messages[0]:
+            options.append(('NOQUEST_NAV_BAK', None, -1))
+
+        options.append(('NOQUEST_NAV_FWD', None, 1))
+
+        action = self.show_choices_message(message_id, *options).wait()
+        return self.step_main_loop, message_index + action.future.result()
