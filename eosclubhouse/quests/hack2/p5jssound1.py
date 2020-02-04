ba184b68b1c0a03320901f9950ab29ceb2683df6
bkarnow <ben@endlessm.com>
2020-02-03 17:20:00 -0800

    quests: P5JSSound1, 2, and 3
    https://phabricator.endlessm.com/T29239

---
+++ eosclubhouse/quests/hack2/p5jssound1.py
@@ -0,0 +1,34 @@
+from eosclubhouse.libquest import Quest
+
+
+class P5JSSound1(Quest):
+
+    __tags__ = ['pathway:art', 'difficulty:medum', 'since:1.5']
+    __pathway_order__ = 620
+
+    def setup(self):
+        self._info_messages = self.get_loop_messages('P5JSSOUND1', start=2)
+
+    def step_begin(self):
+        self.wait_confirm('1')
+        return self.step_launch
+
+    def step_launch(self):
+        self.open_url_in_browser('https://editor.p5js.org/')
+        return self.step_main_loop
+
+    def step_main_loop(self, message_index=0):
+        message_id = self._info_messages[message_index]
+
+        if message_id == self._info_messages[-1]:
+            self.wait_confirm(message_id, confirm_label='See you soon!')
+            return self.step_complete_and_stop
+
+        options = []
+        if message_id != self._info_messages[0]:
+            options.append(('NOQUEST_NAV_BAK', None, -1))
+
+        options.append(('NOQUEST_NAV_FWD', None, 1))
+
+        action = self.show_choices_message(message_id, *options).wait()
+        return self.step_main_loop, message_index + action.future.result()
