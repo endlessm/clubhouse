9b74d02b4312cd1503baa8fa80c6e8dcdd0bc95f
bkarnow <ben@endlessm.com>
2020-03-06 17:59:56 -0800

    quests: Removing auto-open of p5js editor
    https://phabricator.endlessm.com/T29413

--- eosclubhouse/quests/hack2/p5jsintro4.py
+++ eosclubhouse/quests/hack2/p5jsintro4.py
@@ -14,7 +14,6 @@ class P5JSIntro4(Quest):
         return self.step_launch
 
     def step_launch(self):
-        self.open_url_in_browser('https://editor.p5js.org/')
         return self.step_main_loop
 
     def step_main_loop(self, message_index=0):
