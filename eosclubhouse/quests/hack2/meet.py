02836967834b9d9f2691265449c03ad0402f3ea0
bkarnow <ben@endlessm.com>
2020-06-26 12:33:09 -0700

    hook in my quest

--- eosclubhouse/quests/hack2/meet.py
+++ eosclubhouse/quests/hack2/meet.py
@@ -7,6 +7,6 @@ class Meet(InkQuest):
 
     __tags__ = ['pathway:games', 'difficulty:easy']
     __pathway_order__ = 10
-    __ink_quest_id__ = 'test'
+    __ink_quest_id__ = 'kanim2'
     # __ink_quest_id__ = 'p5-quest'
     # __ink_quest_id__ = 'TheIntercept'
