4f2aee0af8301680bd90983864bbb3d474675190
bkarnow <ben@endlessm.com>
2020-07-08 14:49:31 -0700

    hooking in some more test quests

---
+++ eosclubhouse/quests/hack2/testquest1.py
@@ -0,0 +1,8 @@
+from eosclubhouse.libquest import InkQuest
+
+
+class TestQuest1(InkQuest):
+
+    __tags__ = ['pathway:art', 'difficulty:easy']
+    __pathway_order__ = 10
+    __ink_quest_id__ = 'kcell'
---
+++ eosclubhouse/quests/hack2/testquest2.py
@@ -0,0 +1,8 @@
+from eosclubhouse.libquest import InkQuest
+
+
+class TestQuest2(InkQuest):
+
+    __tags__ = ['pathway:art', 'difficulty:easy']
+    __pathway_order__ = 11
+    __ink_quest_id__ = 'kpixel1'
