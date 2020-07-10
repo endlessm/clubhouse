0aa482e969ee0cec84dfa52bee0ad34da34d9f27
bkarnow <ben@endlessm.com>
2020-07-09 20:19:56 -0700

    adding questfile for turtling up

---
+++ eosclubhouse/quests/hack2/testquest3.py
@@ -0,0 +1,8 @@
+from eosclubhouse.libquest import InkQuest
+
+
+class TestQuest3(InkQuest):
+
+    __tags__ = ['pathway:games', 'difficulty:easy']
+    __pathway_order__ = 12
+    __ink_quest_id__ = 'turtlingup1'
