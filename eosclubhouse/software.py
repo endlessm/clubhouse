af0da58851d28187b4c24432b096e1e9dee4ccce
danigm <daniel@endlessm.com>
2020-09-21 17:42:58 +0200

    Update copyright
    Co-authored-by: Philip Withnall <withnall@endlessm.com>

--- eosclubhouse/software.py
+++ eosclubhouse/software.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Endless OS, LLC.
+# Copyright (C) 2020 Endless OS Foundation LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
