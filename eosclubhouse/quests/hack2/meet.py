fad2339a737b609cd51971423f70a038231e4340
Manuel Quiñones <manuel.por.aca@gmail.com>
2020-12-03 14:30:50 -0300

    WIP DSL example, don't merge

--- eosclubhouse/quests/hack2/meet.py
+++ eosclubhouse/quests/hack2/meet.py
@@ -18,68 +18,15 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-from eosclubhouse.libquest import Quest
-from eosclubhouse.utils import ClubhouseState
+from eosclubhouse.libquest import InkQuest
 
 
-class Meet(Quest):
+class Meet(InkQuest):
+
+    __app_id__ = 'com.hack_computer.Sidetrack'
 
     __tags__ = ['pathway:games', 'difficulty:easy']
     __pathway_order__ = 10
-
-    def setup(self):
-        self._clubhouse_state = ClubhouseState()
-
-    def step_begin(self):
-        self._clubhouse_state.characters_disabled = True
-        self.wait_confirm('WELCOME1')
-        self.wait_confirm('WELCOME2')
-        self.highlight_nav('CLUBHOUSE')
-        self.show_hints_message('GOBACK')
-        self.connect_clubhouse_changes(['current-page']).wait()
-        self.highlight_nav('')
-        self.wait_confirm('EXPLAIN_MAIN1')
-        self.wait_confirm('EXPLAIN_MAIN2')
-        return self.step_pathways
-
-    def step_pathways(self):
-        # explain pathways
-        self.highlight_nav('PATHWAYS')
-        self.wait_confirm('EXPLAIN_PATHWAYS1')
-        self.wait_confirm('EXPLAIN_PATHWAYS2')
-        self.highlight_nav('')
-        return self.step_profile
-
-    def step_profile(self):
-        # explain the profile
-        self._clubhouse_state.user_button_highlighted = True
-        for msgid in ['EXPLAIN_PROFILE1', 'EXPLAIN_PROFILE2', 'EXPLAIN_PROFILE3']:
-            self.wait_confirm(msgid)
-        self._clubhouse_state.user_button_highlighted = False
-        # ask if player wants to change their name
-        action = self.show_choices_message('CHANGE_NAME_ASK', ('CHANGE_NAME_YES', None, True),
-                                           ('CHANGE_NAME_NO', None, False)).wait()
-        if action.future.result():
-            for msgid in ['CHANGE_NAME1', 'CHANGE_NAME2', 'CHANGE_NAME3', 'CHANGE_NAME4']:
-                self.wait_confirm(msgid)
-        return self.step_ending
-
-    def step_ending(self):
-        for msgid in ['END1', 'END2']:
-            self.wait_confirm(msgid)
-        self.wait_confirm('END3', confirm_label="See you!")
-        self._clubhouse_state.characters_disabled = False
-        return self.step_complete_and_stop
-
-    def _back_to_normal(self):
-        self._clubhouse_state.characters_disabled = False
-        if not self._clubhouse_state.lights_on:
-            self._clubhouse_state.lights_on = True
-
-    def step_complete_and_stop(self):
-        self._back_to_normal()
-        super().step_complete_and_stop()
-
-    def step_abort(self):
-        self._back_to_normal()
-        super().step_abort()
+    __ink_quest_id__ = 'test'
+    # __ink_quest_id__ = 'p5-quest'
+    # __ink_quest_id__ = 'TheIntercept'
